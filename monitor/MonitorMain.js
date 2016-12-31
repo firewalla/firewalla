7c35906650a91e81c96c618aaf2d24506d0545bc
Jerry Chen <jerryc@gmail.com>
2016-12-31 04:42:59 +0000

    Cleaned up startup routins, they will wait until cloud is active

--- monitor/MonitorMain.js
+++ monitor/MonitorMain.js
@@ -14,12 +14,31 @@
  */
 'use strict';
 
+var bone = require("../lib/Bone.js");
+console.log("================================================================================");
+console.log("Monitor Starting");
+console.log("================================================================================");
+
+run0();
+
+function run0() {
+    if (bone.cloudready()==true) {
+        run();
+    } else {
+        setTimeout(()=>{
+            run0();
+        },1000);
+    }
+}
+
+
+function run() {
+
 let tick = 60 * 15; // waking up every 5 min
 let monitorWindow = 60 * 60 * 8; // eight hours window
 
 let FlowMonitor = require('./FlowMonitor.js');
 let flowMonitor = new FlowMonitor(tick, monitorWindow, 'info');
-
 
 console.log("================================================================================");
 console.log("Monitor Running ");
@@ -45,3 +64,4 @@ setInterval(()=>{
     } catch(e) {
     }
 }, 60*1000);
+}
