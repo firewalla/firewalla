e96ba59436012d1d54123fcbd5e023d83626c557
Melvin Tu <melvinto@gmail.com>
2017-01-06 11:21:37 +0800

    enable ui on api

--- api/lib/CloudWrapper.js
+++ api/lib/CloudWrapper.js
@@ -63,7 +63,8 @@ module.exports = class {
                               let NetBotController = require("../../controllers/netbot.js");
                               let nbConfig = jsonfile.readFileSync(fHome + "/controllers/netbot.json");
                               nbConfig.controller = config.controllers[0];
-                              let nbController = new NetBotController(nbConfig, config, eptcloud, groups, groupID, true, true);
+                              // temp use apiMode = false to enable api to act as ui as well
+                              let nbController = new NetBotController(nbConfig, config, eptcloud, groups, groupID, true, false);
                               if(nbController) {
                                   nbControllers[groupID] = nbController;
                                   console.log("netbot controller for group " + groupID + " is intialized successfully");
