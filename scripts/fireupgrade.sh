357cc9cd890b091a3c7dc0ce3635dbfe45f8188d
Melvin Tu <melvinto@gmail.com>
2017-03-15 18:21:57 +0800

    continue simplify upgrade

--- scripts/fireupgrade.sh
+++ scripts/fireupgrade.sh
@@ -2,6 +2,10 @@
 
 # This script should only handle upgrade, nothing else
 
+if [[ -e "/home/pi/.firewalla/config/.no_auto_upgrade" ]]; then
+  exit 0
+fi
+
 cd /home/pi/firewalla
 cd .git
 sudo chown -R pi *
@@ -13,7 +17,7 @@ GITHUB_STATUS_API=https://status.github.com/api.json
 for i in `seq 1 5`; do
     HTTP_STATUS_CODE=`curl -s -o /dev/null -w "%{http_code}" $GITHUB_STATUS_API`
     if [[ $HTTP_STATUS_CODE == "200" ]]; then
-      break;
+      break
     fi
     sleep 1
 done
@@ -22,16 +26,14 @@ done
 # continue to try upgrade even github api is not successfully.
 # very likely to fail
 
-if [[ ! -e "/home/pi/.firewalla/config/.no_auto_upgrade" ]]; then
-  echo "upgrade on branch $branch"
+echo "upgrade on branch $branch"
 
-  (sudo -u pi git fetch origin $branch && sudo -u pi git reset --hard FETCH_HEAD) || exit 1
+(sudo -u pi git fetch origin $branch && sudo -u pi git reset --hard FETCH_HEAD) || exit 1
 
-  # in case there is some upgrade change on firewalla.service
-  # all the rest services will be updated (in case) via firewalla.service
-  sudo cp /home/pi/firewalla/etc/firewalla.service /etc/systemd/system/.
-  sudo cp /home/pi/firewalla/etc/fireupgrade.service /etc/systemd/system/.
-  sudo systemctl daemon-reload
-  sudo systemctl reenable firewalla
-  sudo systemctl reenable fireupgrade
-fi
+# in case there is some upgrade change on firewalla.service
+# all the rest services will be updated (in case) via firewalla.service
+sudo cp /home/pi/firewalla/etc/firewalla.service /etc/systemd/system/.
+sudo cp /home/pi/firewalla/etc/fireupgrade.service /etc/systemd/system/.
+sudo systemctl daemon-reload
+sudo systemctl reenable firewalla
+sudo systemctl reenable fireupgrade
