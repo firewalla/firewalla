51504e4f7a4695b459c0feb9aa9a163dd9d22b0f
joe <joe>
2017-04-02 10:54:30 +0000

    added touch /dev/watchdog, still comment out

--- scripts/fire-ping.sh
+++ scripts/fire-ping.sh
@@ -12,7 +12,7 @@ DEFAULT_ROUTE=$(ip route show default | awk '/default/ {print $3}')
 for i in `seq 1 3`; do
     if ping -c 1 $DEFAULT_ROUTE &> /dev/null
     then
-       # set /dev/watchdog
+       # sudo touch /dev/watchdog
        /usr/bin/logger "FIREWALLA PING WRITE"
        exit 0
     else
@@ -29,8 +29,11 @@ api_process_cnt=`sudo systemctl status fireapi |grep 'active (running)' | wc -l`
 if [[ $api_process_cnt > 0 ]]; then
    /usr/bin/logger "FIREWALLA PING NO Local Network REBOOT "
    sync
-   echo "SHOULD REBOOT"
    /home/pi/firewalla/scripts/fire-reboot 
+   exit 0
 fi
+
+# sudo touch /dev/watchdog
+/usr/bin/logger "FIREWALLA PING WRITE2"
 
 
