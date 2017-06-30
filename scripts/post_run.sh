d8ebc39dffc9bfd387beec0400894eea792927b7
Melvin Tu <melvinto@gmail.com>
2017-06-30 11:39:38 +0800

    bug fix

--- scripts/post_run.sh
+++ scripts/post_run.sh
@@ -10,7 +10,7 @@ if ! nmap -version | grep "Nmap version 7.40" &>/dev/null; then
         sudo dpkg -i ${FIREWALLA_HOME}/vendor/libssl1.1_1.1.0f-3_armhf.deb
         sudo apt-get install -y liblua5.3-0
         sudo dpkg -i ${FIREWALLA_HOME}/vendor/nmap_7.40-1_armhf.deb
-    elsif [[ $PLATFORM == "x86_64" ]]; then
+    else if [[ $PLATFORM == "x86_64" ]]; then
         sudo dpkg -i ${FIREWALLA_HOME}/vendor/libssl1.1_1.1.0f-3_amd64.deb
         sudo apt-get install -y liblua5.3-0
         sudo dpkg -i ${FIREWALLA_HOME}/vendor/nmap_7.40-1_amd64.deb
