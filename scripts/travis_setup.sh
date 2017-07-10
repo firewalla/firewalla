6ced433bfc9d79fa9a1fe2d2b0cee6e99fca43f9
Melvin Tu <melvinto@gmail.com>
2017-07-11 00:49:16 +0800

    bug fix

--- scripts/travis_setup.sh
+++ scripts/travis_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #find /home/pi/firewalla/bin -name "bitb*" -type f -exec sudo setcap cap_net_admin,cap_net_raw=eip {} \;
-sudo systemctl start redis-server.service
+sudo service redis-server start
