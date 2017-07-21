413e35dd6f089e12063ad40b62139471f09b2360
Melvin Tu <melvinto@gmail.com>
2017-07-21 10:28:18 +0800

    add test cases for device restore

---
+++ scripts/system-reset-all-overlayfs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo service firemain stop
+sudo service firemon stop
+
+# clean database
+/usr/bin/redis-cli flushall
+
+# clean up logs
+: ${FIREWALLA_LOG_DIR:=/log}
+
+sudo rm -fr ${FIREWALLA_LOG_DIR}/*/*
+
+# clean up upper directory
+: ${FIREWALLA_UPPER_DIR:=/media/root-rw/overlay}
+: ${FIREWALLA_UPPER_WORK_DIR:=/media/root-rw/overlay-workdir}
+
+mv ${FIREWALLA_UPPER_DIR}{,.bak}
+mv ${FIREWALLA_UPPER_WORK_DIR}{,.bak}
+
+sync
+sync
+logger "REBOOT: User REBOOT"
+: ${FIREWALLA_REBOOT_NORMAL_SCRIPT:=/home/pi/firewalla/scripts/fire-reboot-normal}
+
+$FIREWALLA_REBOOT_NORMAL_SCRIPT
