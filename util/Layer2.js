a9a4c7f3be73375acbf41692092971bc3baac9b2
Melvin Tu <melvinto@gmail.com>
2017-08-11 01:34:34 +0800

    use the new way to report new device

--- util/Layer2.js
+++ util/Layer2.js
@@ -1,4 +1,4 @@
-/*    Copyright 2016 Firewalla LLC 
+/*    Copyright 2016 Firewalla LLC
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
@@ -20,7 +20,7 @@ let spawn = require('child_process').spawn;
 let log = require('../net2/logger.js')(__filename);
 
 function getMACAndVendor(ipaddress, cb) {
-  
+
   // get MAC Address first
   let mac_address = null;
   let mac_address_vendor = null;
@@ -32,10 +32,10 @@ function getMACAndVendor(ipaddress, cb) {
     } else {
       mac_address = result;
     }
-    
+
     // get MAC OU company
     cb(err, {mac_address: mac_address});
-    /* 
+    /*
     mac.lookup(mac_address, (err, result) => {
       if(err) {
         log.error("Failed to get vendor info for MAC " + mac_address + ", err: " + err);
@@ -48,16 +48,16 @@ function getMACAndVendor(ipaddress, cb) {
     */
   });
 }
-              
+
 
 function getMAC(ipaddress, cb) {
 
   // ping the ip address to encourage the kernel to populate the arp tables
   let ping = spawn("ping", ["-c", "1", ipaddress ]);
-  
+
   ping.on('exit', function (code) {
     // not bothered if ping did not work
-    
+
     let arp = spawn("cat", ["/proc/net/arp"] );
     let buffer = '';
     let errstream = '';
@@ -67,7 +67,7 @@ function getMAC(ipaddress, cb) {
     arp.stderr.on('data', function (data) {
       errstream += data;
     });
-    
+
     arp.on('close', function (code) {
       if (code != 0) {
 	      log.info("Error running arp " + code + " " + errstream);
@@ -75,13 +75,13 @@ function getMAC(ipaddress, cb) {
       }
       let table = buffer.split('\n');
       for ( let l = 0; l < table.length; l++) {
-	
+
 	// parse this format
 	//IP address       HW type     Flags       HW address            Mask     Device
 	//192.168.1.1      0x1         0x2         50:67:f0:8c:7a:3f     *        em1
-	
+
         if (l == 0) continue;
-        
+
         if (table[l].indexOf(ipaddress + " ") == 0) {
           let mac = table[l].substring(41, 58);
           cb(false, mac.toUpperCase());
