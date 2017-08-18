1a6ba9b819c17576594007c190c6154731fb856e
Melvin Tu <melvinto@gmail.com>
2017-08-18 15:05:57 +0800

    bug fix

--- test/test_sysinfo.js
+++ test/test_sysinfo.js
@@ -52,7 +52,7 @@ describe('SysInfo', () => {
         await(delay(1000));
         let info = sysInfo.getSysInfo();
         let threadInfo = info.threadInfo;
-        expect(threadInfo.count).to.below(200);
+        expect(threadInfo.count).to.below(400);
         expect(threadInfo.mainCount).to.below(20);
         expect(threadInfo.monitorCount).to.below(20);
         expect(threadInfo.apiCount).to.below(20);
