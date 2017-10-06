b54d1c02695489c22a63d6b9761651b27e03fdfb
jerry <jerry@firewalla.com>
2017-10-06 00:13:12 +0000

    added event notification

--- scripts/fireupgrade_check.sh
+++ scripts/fireupgrade_check.sh
@@ -31,19 +31,11 @@ latest_tag=$(git describe --tags `git rev-parse origin/$branch`)
 current_version=$(git describe --abbrev=0 --tags)
 latest_version=$(git describe --abbrev=0 --tags `git rev-parse origin/$branch`)
 
-echo $current_tag
-echo $latest_tag
-echo $current_version
-echo $latest_version
-
 IFS=. read -a splited_current_version <<< "$current_version"
 IFS=. read -a splited_latest_version <<< "$latest_version"
 
 current_major_version=${splited_current_version[0]}
 latest_major_version=${splited_latest_version[0]}
-
-echo $current_major_version
-echo $latest_major_version
 
 /home/pi/firewalla/scripts/firelog -t local -m "FIREWALLA.UPGRADECHECK.CHECK Starting $current_tag,$latest_tag,$current_version,$latest_version,$current_major_version,$latest_major_version"
 
@@ -54,9 +46,10 @@ fi
 
 if [ "$current_major_version" == "$latest_major_version" ]; then
    /home/pi/firewalla/scripts/firelog -t local -m "FIREWALLA.UPGRADECHECK.SOFT Starting $current_tag to $latest_tag"
+   redis-cli publish System:Upgrade:Soft $latest_version
    /home/pi/firewalla/scripts/fireupgrade.sh soft
 else
    /home/pi/firewalla/scripts/firelog -t local -m "FIREWALLA.UPGRADECHECK.HARD Starting $current_tag to $latest_tag"
-   redis-cli publish Sys:UpgradeSensor $latest_version
+   redis-cli publish System:Upgrade:Hard $latest_version
    redis-cli set sys:upgrade $latest_version
 fi
