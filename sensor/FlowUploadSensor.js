7889928c13a9a7e2b9f9252e14eb73cc5732ebe9
xuanbei lu <1762175@qq.com>
2017-10-11 12:32:40 +0800

    get all active mac addresses

--- sensor/FlowUploadSensor.js
+++ sensor/FlowUploadSensor.js
@@ -1,11 +1,18 @@
 'use strict';
 
 let util = require('util')
+let async = require('asyncawait/async');
+let await = require('asyncawait/await');
 
 let log = require('../net2/logger.js')(__filename)
 let Sensor = require('./Sensor.js').Sensor
 
+let HostManager = require('../net2/HostManager.js');
+let hostManager = new HostManager('cli', 'server');
 
+let INTERVAL_MIN = 10 //10 seconds
+let INTERVAL_MAX = 3600 //1 hour
+let INTERVAL_DEFAULT = 900 //15 minutes
 
 class FlowUploadSensor extends Sensor {
     constructor() {
@@ -15,8 +22,8 @@ class FlowUploadSensor extends Sensor {
     run() {
         this.startTime = new Date() / 1000
         //do some protection if interval configured by mistake
-        if (this.config.interval < 10 || this.config.interval > 86400) {
-            this.config.interval = 900 //15 minutes
+        if (this.config.interval < INTERVAL_MIN || this.config.interval > INTERVAL_MAX) {
+            this.config.interval = INTERVAL_DEFAULT
         }
         log.info("schedule started with interval " + this.config.interval + " seconds")
         setInterval(() => {
@@ -30,7 +37,33 @@ class FlowUploadSensor extends Sensor {
         log.info("start to upload flow from "
          + this.startTime + "(" + new Date(this.startTime * 1000).toTimeString() + ")" + 
          " to " + endTime + "(" + new Date(endTime * 1000).toUTCString() + ")")
-        this.startTime = endTime + 0.001
+
+         return async(() => {
+            try {
+                await(this.getFlow(this.startTime, endTime))
+                log.info("Upload flow to cloud complete");
+                this.startTime = endTime + 0.001
+            } catch (err) {
+                log.error("something wrong when getting flow" + err.toString())
+            }
+          })();
+    }
+
+    getFlow(start, end) {
+        if (end - start < INTERVAL_MIN) {
+            return Promise.reject(new Error("Get flow too soon ("  + (end - start) + " seconds)"));
+        }
+        return async(() => {
+            let macs = this.getQualifiedDevices();
+            macs.forEach((mac) => {
+              
+            })
+          })();
+    }
+
+    // return a list of mac addresses that's active in last xx days
+    getQualifiedDevices() {
+        return hostManager.hosts.all.map(h => h.o.mac).filter(mac => mac != null);
     }
 }
 
