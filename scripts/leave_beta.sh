457ca1a5efd920ef13d0a8205b617ce0c310cb9b
Melvin Tu <melvinto@gmail.com>
2017-10-29 15:03:19 +0800

    add join/leave beta

--- scripts/leave_beta.sh
+++ scripts/leave_beta.sh
@@ -2,10 +2,21 @@
 
 : ${FIREWALLA_HOME:=/home/pi/firewalla}
 
-cd $FIREWALLA_HOME
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-git fetch origin
-git checkout -
-sync
-logger "REBOOT: Leave Beta"
-/home/pi/firewalla/scripts/fire-reboot-normal
+prod_branch=$(redis-cli get prod.branch)
+
+if [[ ! -z $prod_branch ]]; then
+  cd $FIREWALLA_HOME
+  git config remote.origin.fetch "+refs/heads/${prod_branch}:refs/remotes/origin/${prod_branch}"
+  git fetch origin
+  git checkout $prod_branch
+
+  cd ~/.node_modules
+  git config remote.origin.fetch "+refs/heads/${prod_branch}:refs/remotes/origin/${prod_branch}"
+  git fetch origin
+  git checkout $prod_branch
+  
+  sync
+  logger "REBOOT: Leave Beta"
+  /home/pi/firewalla/scripts/fire-reboot-normal  
+fi
+
