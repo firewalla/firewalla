ead5c088f6790f9ec701f94de0f2d5bef2f13eea
Melvin Tu <melvinto@gmail.com>
2017-11-20 18:50:53 +0800

    bug fix

--- scripts/app_finder.js
+++ scripts/app_finder.js
@@ -21,24 +21,59 @@ const program = require('commander');
 const async = require('asyncawait/async');
 const await = require('asyncawait/await');
 
+const redis = require('redis')
+const rclient = redis.createClient()
+
+const Promise = require('bluebird')
+Promise.promisifyAll(redis.RedisClient.prototype);
+
+
 const flowTool = require('../net2/FlowTool.js')()
 
+const IntelTool = require('../net2/IntelTool.js')
+const intelTool = new IntelTool()
+
 program.version('0.0.1')
-  .option('--ip [ip]', 'ip to list app/category activity');
+  .option('--ip [ip]', 'ip to list app/category activity')
+  .option('--app [app]', 'app')
+  .option('--category [category]', 'category')
+
 
 program.parse(process.argv);
+
+function print(flow, app, category) {
+  let d = new Date(0)
+  d.setUTCSeconds(flow.ts)
+ 
+  log.info(d, app, category, flow.sh, flow.dh, flow.ob, flow.rb, {})
+}
 
 if(program.ip) {
   let ip = program.ip
 
   async(() => {
-    
-    let flows = await (flowTool.getRecentOutgoingConnections(ip, {
-      maxRecentFlow: 10000
-    }))
 
-    console.log(flows)
+    let key = util.format("flow:conn:%s:%s", 'in', ip);
 
+    let flows = await (rclient.zrangebyscoreAsync(key, 0, -1))
+
+    for(let i in flows) {
+      let flowJSON = flows[i]
+      let flow = JSON.parse(flowJSON)
+
+      let srcIP = flow.sh
+      let destIP = flow.dh
+
+      let intel = await (intelTool.getIntel(ip))
+
+      if(program.app && intel.app === program.app) {
+        print(flow, program.app, null)
+      }
+
+      if(program.category && intel.category === program.category) {
+        print(flow, null, program.category)
+      }
+    }
   })()
 } else {
   process.exit(0)
