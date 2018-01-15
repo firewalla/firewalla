ba273cf26d974f96814cd94f54cbdf37f10317e9
Melvin Tu <melvinto@gmail.com>
2018-01-15 15:19:42 +0800

    add exception and rejection handling to all nodes

--- monitor/MonitorMain.js
+++ monitor/MonitorMain.js
@@ -55,8 +55,19 @@ process.on('uncaughtException',(err)=>{
     }
     bone.log("error",{version:config.version,type:'FIREWALLA.MON.exception',msg:err.message,stack:err.stack},null);
     setTimeout(()=>{
+        require('child_process').execSync("touch /home/pi/.firewalla/managed_reboot")
         process.exit(1);
     },1000*2);
+});
+
+process.on('unhandledRejection', (reason, p)=>{
+  let msg = "Possibly Unhandled Rejection at: Promise " + p + " reason: "+ reason;
+  log.warn('###### Unhandled Rejection',msg,reason.stack,{});
+  bone.log("warn",{version:config.version,type:'FIREWALLA.MON.unhandledRejection',msg:msg,stack:reason.stack},null);
+  setTimeout(()=>{
+    require('child_process').execSync("touch /home/pi/.firewalla/managed_reboot")
+    process.exit(1);
+  },1000*2);
 });
 
 let heapSensor = null;
