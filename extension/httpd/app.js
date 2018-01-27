71144361b23ff775b8b1af742122645cf4f0a267
Alex Song <starsy@gmail.com>
2018-01-28 00:37:20 +0800

    generate https options on-the-fly

--- extension/httpd/app.js
+++ extension/httpd/app.js
@@ -2,17 +2,20 @@
 
 const express = require('express');
 const app = express();
-const port = 80;
-const httpsPort = 443;
+const port = 18080;
+const httpsPort = 8443;
 const path = require('path');
 const certPath = 'extension/httpd';
 const fs = require('fs');
 const https = require('https');
+const forge = require('node-forge');
 
-const httpsOptions = {
-    key: fs.readFileSync(path.join(certPath, "domain.key")),
-    cert: fs.readFileSync(path.join(certPath, "domain.crt"))
-};
+let f = require('../../net2/Firewalla.js');
+let fHome = f.getFirewallaHome();
+let userID = f.getUserID();
+let confDir = f.getUserConfigFolder() + "/httpd";
+
+const httpsOptions = genHttpsOptions();
 
 app.use('*', (req, res) => {
   let txt = `Ads Blocked by Firewalla: ${req.ip} => ${req.method}: ${req.hostname}${req.originalUrl}`;
@@ -23,3 +26,76 @@ app.use('*', (req, res) => {
 app.listen(port, () => console.log(`Httpd istening on port ${port}!`));
 
 https.createServer(httpsOptions, app).listen(httpsPort, () => console.log(`Httpd istening on port ${httpsPort}!`));
+
+
+function genHttpsOptions() {
+// generate a keypair and create an X.509v3 certificate
+  const pki = forge.pki;
+  console.log('Generating 1024-bit key-pair...');
+  const keys = pki.rsa.generateKeyPair(1024);
+  console.log('Key-pair created.');
+
+  console.log('Creating self-signed certificate...');
+  const cert = pki.createCertificate();
+  cert.publicKey = keys.publicKey;
+  cert.serialNumber = '01';
+  cert.validity.notBefore = new Date();
+  cert.validity.notAfter = new Date();
+  cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
+  let attrs = [{
+    name: 'commonName',
+    value: 'blackhole.firewalla.com'
+  }, {
+    name: 'countryName',
+    value: 'US'
+  }, {
+    shortName: 'ST',
+    value: 'New York'
+  }, {
+    name: 'localityName',
+    value: 'Brooklyn'
+  }, {
+    name: 'organizationName',
+    value: 'BLACKHOLE'
+  }, {
+    shortName: 'OU',
+    value: 'BLACKHOLE'
+  }];
+  cert.setSubject(attrs);
+  cert.setIssuer(attrs);
+  cert.setExtensions([{
+    name: 'basicConstraints',
+    cA: true/*,
+  pathLenConstraint: 4*/
+  }, {
+    name: 'keyUsage',
+    keyCertSign: true,
+    digitalSignature: true,
+    nonRepudiation: true,
+    keyEncipherment: true,
+    dataEncipherment: true
+  }, {
+    name: 'extKeyUsage',
+    serverAuth: true,
+    clientAuth: true,
+    codeSigning: true,
+    emailProtection: true,
+    timeStamping: true
+  }, {
+    name: 'nsCertType',
+    client: true,
+    server: true,
+    email: true,
+    objsign: true,
+    sslCA: true,
+    emailCA: true,
+    objCA: true
+  }]);
+  cert.sign(keys.privateKey);
+  console.log('Certificate created.');
+
+  return {
+    key: pki.privateKeyToPem(keys.privateKey),
+    cert: pki.certificateToPem(cert)
+  };
+}
