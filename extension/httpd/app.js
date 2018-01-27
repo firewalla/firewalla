74db5d9a1248dcc4696dabb5224b863eaf432e68
Alex Song <starsy@gmail.com>
2018-01-27 15:50:01 +0000

    support https

--- extension/httpd/app.js
+++ extension/httpd/app.js
@@ -1,9 +1,24 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const port = 80;
+const httpsPort = 443;
+const path = require('path');
+const certPath = 'extension/httpd';
+const fs = require('fs');
+const https = require('https');
 
-app.use('/*', (req, res) => {
-  res.send('Ads Blocked by Firewalla');
+const httpsOptions = {
+    key: fs.readFileSync(path.join(certPath, "domain.key")),
+    cert: fs.readFileSync(path.join(certPath, "domain.crt"))
+};
+
+app.use('*', (req, res) => {
+  let txt = `Ads Blocked by Firewalla: ${req.method} ${req.hostname}${req.originalUrl}`;
+  res.send(txt);
 });
 
-app.listen(port, () => console.log(`Httpd istening on port ${port}!`))
+app.listen(port, () => console.log(`Httpd istening on port ${port}!`));
+
+https.createServer(httpsOptions, app).listen(httpsPort, () => console.log(`Httpd istening on port ${httpsPort}!`));
