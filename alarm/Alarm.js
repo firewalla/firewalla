5ca5cde9f557627771649e5b2503f322a88fbcb0
Melvin Tu <melvinto@gmail.com>
2018-02-28 11:04:52 +0800

    fix notif template

--- alarm/Alarm.js
+++ alarm/Alarm.js
@@ -41,12 +41,7 @@ class Alarm {
   }
 
   getNotificationCategory() {
-    if(this.result === "block" &&
-       this.result_method === "auto") {
-      return "NOTIF_AUTO_BLOCK_" + this.getI18NCategory();
-    } else {
-      return "NOTIF_" + this.getI18NCategory();
-    }
+    return "NOTIF_" + this.getI18NCategory();
   }
 
   getI18NCategory() {
