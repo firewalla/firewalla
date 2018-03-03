1ed632994ee34e0f5ae1bb3fbb5df03f43f43147
starsy <starsy@gmail.com>
2018-03-03 23:49:28 +0800

    completely remove bone intel

--- extension/httpd/intel.js
+++ extension/httpd/intel.js
@@ -15,27 +15,11 @@ class Intel {
       this.types = (await Promise.map(await redis.keysAsync('dns:hashset:*'), key => key.split(':')[2])).filter(x => x);
     })();
   }
-  
-  async jwt() {
-    try {
-      return await redis.getAsync("sys:bone:jwt");
-    } catch (err) {
-      // null
-    }
-  }
 
   async check(dn) {
     try {
       let intel = await this.checkIntelLocally(dn);
       log.info(`local intel for ${dn} is: ${intel}`);
-
-      if (intel) {
-        return intel;
-      }
-
-      intel = await this.checkIntelFromCloud(dn);
-      log.info(`cloud intel for ${dn} is: ${intel}`);
-
       return intel;
     } catch (err) {
       log.error("Error when check intel:", err, {});
@@ -68,71 +52,6 @@ class Intel {
       .reduce((acc, cur) => Object.assign(acc, {[cur.type]: cur.isMember}), {});
   };
 
-  async checkIntelFromCloud(dn) {
-    log.info("Checking intel for", dn);
-    //log.debug(`JWT: ${bone.getToken()}`);
-
-    let origHost = {};
-    let debugMode = false;
-
-    let hds = flowUtil.hashHost(dn, {keepOriginal: true});
-    hds.forEach(hd => origHost[hd[2]] = hd[0]);
-
-    let _hlist = hds.map(x => x.slice(1, 3));
-    let _alist = flowUtil.hashApp(dn);
-
-    //log.debug('Mapping: ' + util.inspect(origHost));
-
-    let alist = [dn], hlist = [dn], iplist = [dn];
-    let _iplist = _hlist;
-    let flow = {fd: 'in'};
-
-    let flowlist = debugMode ?
-      [{iplist, hlist, alist, _iplist, _hlist, _alist, flow}] :
-      [{_iplist, _hlist, _alist, flow}];
-
-    let data = {flowlist, hashed: 1};
-
-    //log.info(util.inspect(data, {depth: null}));
-
-    let results, best;
-
-    try {
-      results = await this.intel("*", "", "check", data);
-    } catch (err) {
-      log.error('Unable to get intel from cloud', err, {});
-    }
-
-    if (Array.isArray(results) && results.length > 0) {
-      best = results.reduce((best, cur) => origHost[cur.ip].length > origHost[best.ip].length ? cur : best);
-    }
-
-    return best ? best.c : null;
-  }
-
-  async intel(ip, type, action, intel) {
-    log.debug("/intel/host/" + ip + "/" + action);
-    let options = {
-      uri: getEndpoint() + '/intel/host/' + ip + '/' + action,
-      family: 4,
-      method: 'POST',
-      auth: {
-        bearer: await this.jwt()
-      },
-      json: intel,
-      timeout: 10000 // 10 seconds
-    };
-
-    return new Promise((resolve, reject) => {
-      request(options, (err, httpResponse, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(body);
-        }
-      });
-    });
-  }
 }
 
 module.exports = new Intel();
