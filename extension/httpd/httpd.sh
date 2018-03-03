4ed476171c14b2adb06223c9c0143b9afb00a421
starsy <starsy@gmail.com>
2018-03-04 00:47:22 +0800

    expose gc incase need agressive memory mgmt

--- extension/httpd/httpd.sh
+++ extension/httpd/httpd.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 export NODE_PATH=/home/pi/.node_modules:$NODE_PATH
-/home/pi/firewalla/bin/node $DIR/app.js
+/home/pi/firewalla/bin/node --expose-gc $DIR/app.js
