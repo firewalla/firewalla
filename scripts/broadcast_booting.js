abc794f6e509c2cf179be708cad61131e416ed1c
Melvin Tu <melvinto@gmail.com>
2018-03-03 20:47:48 +0800

    add a booting bonjour msg

--- scripts/broadcast_booting.js
+++ scripts/broadcast_booting.js
@@ -16,12 +16,56 @@
 'use strict'
 
 const bonjour = require('bonjour')()
- 
-// advertise an HTTP server on port 3000 
-bonjour.publish({ name: 'Firewalla Booting', type: 'http', port: 18833 })
- 
-setTimeout(() => {
+const cp = require('child_process')
+
+const cmd = "ip addr show dev eth0 | awk '/inet / {print $2}'|cut -f1 -d/ | grep -v 192.168.218.1 | head -n 1"
+const ip = cp.execSync(cmd).toString().replace(/\n$/, '')
+
+const cmd3 = "redis-cli hget sys:ept gid"
+const gid = cp.execSync(cmd3).toString().replace(/\n$/, '')
+
+function exitBooting() {
   bonjour.unpublishAll((err) => {
     process.exit(0)
   })
-}, 5 * 60 * 1000)
+}
+
+/*
+        const cmd2 = "pgrep FireKick"
+        const cnt = cp.execSync(cmd2).toString().replace(/\n$/, '')
+function checkBinding() {
+
+        if(cnt != "" && cnt > 1) {
+                exitBooting()
+        }
+}
+*/
+
+
+let txt = {
+  ip: ip,
+  exp: Math.floor(new Date() / 1000) + 5 * 60
+}
+
+if (gid && gid != "") {
+  txt.gid = gid
+}
+
+// advertise an HTTP server on port 3000
+bonjour.publish({
+  name: 'Firewalla Booting' + Math.floor(Math.random() * 1000),
+  type: 'http',
+  port: 18833,
+  txt: txt
+})
+
+
+/*
+setInterval(() => {
+        checkBinding()
+}, 10 * 1000)
+*/
+
+setTimeout(() => {
+  exitBooting()
+}, 15 * 60 * 1000)
