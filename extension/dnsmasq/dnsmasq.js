50b901c1dcb0998e9d41244ed7b0fdfbb7c5182a
Melvin Tu <melvinto@gmail.com>
2018-03-24 22:18:36 +0800

    bug fix

--- extension/dnsmasq/dnsmasq.js
+++ extension/dnsmasq/dnsmasq.js
@@ -60,7 +60,7 @@ let dnsmasqBinary = __dirname + "/dnsmasq";
 let dnsmasqPIDFile = f.getRuntimeInfoFolder() + "/dnsmasq.pid";
 let dnsmasqConfigFile = __dirname + "/dnsmasq.conf";
 
-let dnsmasqResolvFile = f.getRuntimeInfoFolder() + "/dnsmasq.resolv.conf";
+let dnsmasqResolvFile = f.getRuntimeInfoFolder() + "/dnsmasq.resolv.conf"
 
 let defaultNameServers = {};
 let upstreamDNS = null;
@@ -186,8 +186,14 @@ module.exports = class DNSMASQ {
     let entries = nameservers.map((nameserver) => "nameserver " + nameserver);
     let config = entries.join('\n');
     config += "\n";
-    fs.writeFileSync(dnsmasqResolvFile, config);
-    callback(null);
+
+    async(() => {
+      await (fs.writeFileAsync(dnsmasqResolvFile, config))
+      await (exec("pkill -SIGHUP dnsmasq"))
+      callback(null)
+    })().catch((err) => {
+      callback(err)
+    })    
   }
 
   updateFilter(type, force, callback) {
