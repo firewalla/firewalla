2bae3ff18fbf7b13749d88d78e7951c9a7fe2854
Alex Song <starsy@gmail.com>
2018-05-25 16:48:23 +0800

    refactor

--- util/DomainCategory.js
+++ util/DomainCategory.js
@@ -4,25 +4,16 @@ const rp = require('request-promise');
 const log = require("../net2/logger.js")(__filename);
 
 class DomainCategory {
-  constructor() {
-    this.baseurl = "http://sitereview.bluecoat.com/resource/lookup"
-    this.useragent = "Mozilla/5.0";
-    this.timeout = 10000;
-  }
-  
   // callback: function(category) { ... };
   async getCategory(url) {
-    let options = {
-      uri: this.baseurl,
+    return rp.post({
+      uri: "http://sitereview.bluecoat.com/resource/lookup",
       headers: {
-        'User-Agent': this.useragent,
+        'User-Agent': "Mozilla/5.0",
       },
-      method: 'POST',
       body: {url, captcha: ''},
-      timeout: this.timeout
-    };
-
-    rp.post(options).then(body => {
+      timeout: 10000 //ms
+    }).then(body => {
       let category = null;
       try {
         let _body = JSON.parse(body);
@@ -31,11 +22,11 @@ class DomainCategory {
           category = _body.categorization[0];
         }
       } catch (err) {
-        log.error('unable to obtain category', err, {});
+        log.error('unable to obtain category', err);
       }
       return category;
     }).catch(err => {
-        log.error('error when query domain category', err, {});
+      log.error('error when query domain category', err);
     });
   }
 }
