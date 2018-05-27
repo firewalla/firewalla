5c5ec4860c8095d4b6758f5aa7b106e3b27a461f
starsy <starsy@gmail.com>
2018-05-27 21:56:10 +0800

    add whois service

--- controllers/netbot.js
+++ controllers/netbot.js
@@ -1515,6 +1515,15 @@ class netBot extends ControllerBot {
           this.simpleTxData(msg, {}, err, callback)
         })
         break
+      case "whois":
+        (async () => {
+          const target = msg.data.value.target;
+          let whois = intelManager.whois(target);
+          this.simpleTxData(msg, {target, whois}, null, callback)
+        })().catch((err) => {
+          this.simpleTxData(msg, {}, err, callback)
+        })
+        break;
     default:
         this.simpleTxData(msg, null, new Error("unsupported action"), callback);
     }
