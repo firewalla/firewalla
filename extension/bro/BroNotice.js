db7b94e3f063297a00ab62b319f32c7c16b10320
Melvin Tu <melvinto@gmail.com>
2018-06-23 01:45:46 +0800

    add heartbleed hanlding

---
+++ extension/bro/BroNotice.js
@@ -0,0 +1,92 @@
+/*    Copyright 2016 Firewalla LLC / Firewalla LLC
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict';
+let instance = null;
+
+const SysManager = require('../../net2/SysManager.js')
+const sysManager = new SysManager();
+
+class BroNotice {
+  constructor() {
+    if(instance === null) {
+      instance = this;
+    }
+  }
+
+  async processSSHScan(alarm) {
+    const subMessage = obj.sub
+    // sub message:
+    //   Sampled servers:  10.0.1.182, 10.0.1.182, 10.0.1.182, 10.0.1.182, 10.0.1.182
+    
+    let addresses = subMessage.replace(/.*Sampled servers:  /, '').split(", ")
+    addresses = addresses.filter((v, i, array) => {
+      return array.indexOf(v) === i
+    })
+
+    if(addresses.length > 0) {
+      const ip = addresses[0];
+      alarm["p.device.ip"] = ip;
+      alarm["p.device.name"] = ip;
+      const mac = await hostTool.getMacByIP(ip);
+      if(mac) {
+        alarm["p.device.mac"] = mac;
+      }
+    }
+
+    alarm["p.message"] = `${alarm["p.message"].replace(/\.$/, '')} on device: ${addresses.join(",")}`
+  }
+
+  async processHeartbleed(alarm) {
+    const from = alarm["src"];
+    const to = alarm["dst"];
+
+    let localIP = null;
+    // initiated from myself
+    if(sysManager.isLocalIP(from)) {
+      alarm["local_is_client"] = 1;
+      localIP = from;
+    } else {
+      // initiated from outside
+      alarm["local_is_client"] = 0;
+      alarm["p.action.block"] = true; // block automatically if initiated from outside in
+      localIP = to;
+    }
+  }
+
+  async processNotice(alarm) {
+    const noticeType = alarm["p.noticeType"];
+
+    if(!noticeType) {
+      return;
+    }
+
+    switch(noticeType) {
+      case "SSH::Password_Guessing":
+      await this.processSSHScan(alarm);
+      break;
+
+      case "Heartbleed::SSL_Heartbeat_Attack":
+      await this.processHeartbleed(alarm);
+      break;
+      
+      default:
+      // do nothing
+      break;
+    }
+  }
+};
+
+module.exports = new BroNotice();
