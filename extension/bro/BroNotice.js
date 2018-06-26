d00a96597e87acab010df5c3f0021fc4b0eb1eaa
Melvin Tu <melvinto@gmail.com>
2018-06-26 18:12:21 +0800

    bug fix

--- extension/bro/BroNotice.js
+++ extension/bro/BroNotice.js
@@ -19,6 +19,9 @@ let instance = null;
 const SysManager = require('../../net2/SysManager.js')
 const sysManager = new SysManager();
 
+const HostTool = require('../net2/HostTool.js');
+const hostTool = new HostTool();
+
 const log = require('../../net2/logger.js')(__filename);
 
 class BroNotice {
