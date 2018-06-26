604f6c79613967abe697aed587c0cdc696bd0ef5
Melvin Tu <melvinto@gmail.com>
2018-06-27 04:24:58 +0800

    update alias.sh

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -29,4 +29,5 @@ alias fufu='sudo -u pi git fetch origin $branch && sudo -u pi git reset --hard F
 alias node='/home/pi/firewalla/bin/node'
 alias fuc='/home/pi/firewalla/scripts/fireupgrade_check.sh'
 alias srr='/home/pi/firewalla/scripts/main-run'
+alias srrr='/home/pi/firewalla/scripts/fireupgrade_check.sh'
 alias rc='redis-cli'
