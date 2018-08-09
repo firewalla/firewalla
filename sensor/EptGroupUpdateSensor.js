432d168aeb4b687020b04c5695230e0593cde417
Melvin Tu <melvinto@gmail.com>
2018-08-10 01:16:05 +0800

    add ept group update sensor

---
+++ sensor/EptGroupUpdateSensor.js
@@ -0,0 +1,39 @@
+/*    Copyright 2016 Firewalla LLC
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+'use strict';
+const log = require('../net2/logger.js')(__filename);
+
+const Sensor = require('./Sensor.js').Sensor;
+
+const sem = require('../sensor/SensorEventManager.js').getInstance();
+
+const Promise = require('bluebird');
+
+class EptGroupUpdateSensor extends Sensor {
+  constructor() {
+    super();    
+  }
+
+  job() {
+  }
+
+  run() {
+    setInterval(() => {
+      this.job();
+    }, this.config.interval || 1000 * 60 * 30); // every thirty minutes
+  }
+}
+
+module.exports = EptGroupUpdateSensor
