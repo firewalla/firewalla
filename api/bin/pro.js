7c53ca475606b49874d5d4f4aed4935349a64fb4
duzitong <duzitong811@126.com>
2018-08-28 22:12:24 +0800

    fix port binding on device

--- api/bin/pro.js
+++ api/bin/pro.js
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
 
-import { start } from 'repl';
-
 'use strict'
 
 let http = require('http');
 let ip = require("ip");
+let log = require('../../net2/logger')(__filename, "info");
 
 let app = require('../app-pro');
-let port = normalizePort(process.env.PRO_PORT || '8835');
+let port = process.env.PRO_PORT || '8832';
 app.set('port', port);
 let proServer = http.createServer(app);
 let started = false;
+let iface = process.env.NETWORK_INTERFACE || 'eth0'
 
 function startProServer(onlisten) {
     if (!started) {
-        proServer.listen(port, ip.address(), () => {started = true; onlisten(true)});
+        proServer.listen(port, ip.address(iface), () => {
+            started = true;
+            log.info('Pro API listening on ', ip.address(iface), port);
+            onlisten(true)
+        });
     } else {
         onlisten(false);
     }
