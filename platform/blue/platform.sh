5416376889b7957280cc97817ffcbd71fd4d3e96
Melvin Tu <melvinto@gmail.com>
2018-09-13 12:31:22 +0800

    support custmozed bro config per platform

--- platform/blue/platform.sh
+++ platform/blue/platform.sh
@@ -11,3 +11,4 @@ function heartbeatLED {
   sudo sh -c 'echo none > /sys/devices/platform/leds/leds/nanopi:green:status/trigger'
   sudo sh -c 'echo heartbeat > /sys/devices/platform/leds/leds/nanopi:red:pwr/trigger'
 }
+
--- platform/platform.sh
+++ platform/platform.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+FW_PLATFORM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 UNAME=$(uname -m)
 
 case "$UNAME" in
 "x86_64")
-  source $DIR/docker/platform.sh
+  source $FW_PLATFORM_DIR/docker/platform.sh
+  FW_PLATFORM_CUR_DIR=$FW_PLATFORM_DIR/docker
   ;;
 "aarch64")
-  source $DIR/blue/platform.sh
+  source $FW_PLATFORM_DIR/blue/platform.sh
+  FW_PLATFORM_CUR_DIR=$FW_PLATFORM_DIR/blue
   ;;
 "armv7l")
-  source $DIR/red/platform.sh
+  source $FW_PLATFORM_DIR/red/platform.sh
+  FW_PLATFORM_CUR_DIR=$FW_PLATFORM_DIR/red
   ;;
 *)
   ;;
 esac
+
+
+function before_bro {
+  if [[ -d ${FW_PLATFORM_CUR_DIR}/before_bro]]; then
+    for script in `ls -1 ${FW_PLATFORM_CUR_DIR}/before_bro/*.sh`; do
+      $script
+    done
+  fi
+}
