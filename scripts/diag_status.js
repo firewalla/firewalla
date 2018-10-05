af26d72fcc7656bf72f70536f21997b15c83bfad
Melvin Tu <melvinto@gmail.com>
2018-10-06 02:49:39 +0800

    add new diag script based on nodejs

---
+++ scripts/diag_status.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const fwDiag = require("../extension/install/diag.js");
+
+program.version('0.0.1')
+  .option('--event [event]', 'event')
+  .option('--message [message]', 'message')
+
+program.parse(process.argv);
+
+if(!program.event || !program.message) {
+  console.log("parameters event and message are required");
+  exit(0);
+}
+
+(async () => {
+  await fwDiag.submitInfo({
+    event: program.event,
+    msg: program.message
+  });
+  process.exit(0);
+})
