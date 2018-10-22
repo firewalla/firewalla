8069a9a2e3fda39e9e69ba54d5bf1efbdfdf43cb
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2018-10-22 17:58:49 +0800

    update nat for customized dhcp subnet

--- net2/SecondaryInterface.js
+++ net2/SecondaryInterface.js
@@ -66,6 +66,8 @@ exports.create = function (config, callback) {
         let _secondaryIpSubnet = config.secondaryInterface.ipsubnet;
         let _secondaryIpNet = config.secondaryInterface.ipnet;
         let _secondaryMask = config.secondaryInterface.ipmask;
+        let legacyIp = null;
+        let legacySubnet = null;
         linux.get_network_interfaces_list((err,list)=>{
             list = list.filter(function(x) { return is_interface_valid(x) });
             for (let i in list) {
@@ -73,10 +75,13 @@ exports.create = function (config, callback) {
                     if (list[i].netmask === "Mask:" + _secondaryMask && list[i].ip_address === _secondaryIp.split('/')[0]) {
                         // same ip and subnet mask
                         log.info("Already Created Secondary Interface",list[i]);
-                        callback(null,_secondaryIp, _secondaryIpSubnet,_secondaryIpNet, _secondaryMask);
+                        callback(null,_secondaryIp, _secondaryIpSubnet,_secondaryIpNet, _secondaryMask, legacyIp, legacySubnet);
                         return;
                     } else {
                         log.info("Update existing secondary interface: " + config.secondaryInterface.intf);
+                        const legacyIpSubnet = ip.subnet(list[i].ip_address, list[i].netmask.substring(5));
+                        legacyIp = legacyIpSubnet.firstAddress + "/" + legacyIpSubnet.subnetMaskLength; // should be like 192.168.218.1/24
+                        legacySubnet = legacyIpSubnet.networkAddress + "/" + legacyIpSubnet.subnetMaskLength; // should be like 192.168.218.0/24
                     }
                 } else {
                     // other interface already occupies ip1, use alternative ip
@@ -97,7 +102,7 @@ exports.create = function (config, callback) {
                 require('child_process').exec("sudo /home/pi/firewalla/scripts/config_secondary_interface.sh "+_secondaryIp,(err,out,code)=>{
                 });
                 if (callback) {
-                    callback(err,_secondaryIp, _secondaryIpSubnet, _secondaryIpNet, _secondaryMask);
+                    callback(err,_secondaryIp, _secondaryIpSubnet, _secondaryIpNet, _secondaryMask, legacyIp, legacySubnet);
                 }
             });
          });
