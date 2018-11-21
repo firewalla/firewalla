c0596246541a718d465c6ca577ced524d15afa5b
Reinhard <callmerein@gmail.com>
2018-11-21 18:23:33 +0800

    revert change in fireupgrade_check.sh

--- scripts/fireupgrade_check.sh
+++ scripts/fireupgrade_check.sh
@@ -55,5 +55,4 @@ else
    /home/pi/firewalla/scripts/firelog -t local -m "FIREWALLA.UPGRADECHECK.HARD Starting $current_tag to $latest_tag"
    redis-cli publish System:Upgrade:Hard $latest_version
    redis-cli set sys:upgrade $latest_version
-   /home/pi/firewalla/scripts/fireupgrade.sh hard
 fi
