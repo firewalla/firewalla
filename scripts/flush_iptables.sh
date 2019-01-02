6b3b686a2c3692459f614223a181915a7ebab8e5
Reinhard <callmerein@gmail.com>
2019-01-02 23:26:46 +0800

    keep DNAT & MASQUERADE rules on DHCP mode

--- scripts/flush_iptables.sh
+++ scripts/flush_iptables.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
+# if the box is in dhcp mode, we will want to keep DNAT rules alive as much as possible
+# or the user's internet connection will go dead
+
+MODE=$(redis-cli get mode)
+if [ "$MODE" = "dhcp" ]
+then
+  mapfile -t DNAT_RULES < <( sudo iptables -t nat -S | grep DNAT ) # Backup DNAT rules into DNAT_RULES as an array
+  mapfile -t MASQ_RULES < <( sudo iptables -t nat -S | grep MASQUERADE )
+  DHCP_RULES=("${DNAT_RULES[@]}" "${MASQ_RULES[@]}") # Merge 2 arrays
+fi
+
 sudo iptables -w -F
 sudo iptables -w -F -t nat
 sudo iptables -w -F -t raw
 sudo ip6tables -w -F
 sudo ip6tables -w -F -t nat
 sudo ip6tables -w -F -t raw
+
+if [ "$MODE" = "dhcp" ]
+then
+  for RULE in "${DHCP_RULES[@]}";
+  do 
+    sudo iptables -w -t nat $RULE
+  done
+fi
