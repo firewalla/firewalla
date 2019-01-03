cbc9cab80d2e99073fe214d35da14dba2ccaedf2
Reinhard <callmerein@gmail.com>
2019-01-03 19:50:33 +0800

    remove DNAT rules, add SNAT rules

--- scripts/flush_iptables.sh
+++ scripts/flush_iptables.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# if the box is in dhcp mode, we want to keep DNAT/MASQUERADE rules alive as much as possible
+# if the box is in dhcp mode, we want to keep MASQUERADE rule alive as much as possible
 # or user's internet connection will go dead
-
+# DNAT rules are free to flush as they won't block internet
 MODE=$(redis-cli get mode)
 if [ "$MODE" = "dhcp" ]
 then
-  mapfile -t DNAT_RULES < <( sudo iptables -t nat -S | grep DNAT ) # Backup DNAT rules into DNAT_RULES as an array
-  mapfile -t MASQ_RULES < <( sudo iptables -t nat -S | grep MASQUERADE )
-  DHCP_RULES=("${DNAT_RULES[@]}" "${MASQ_RULES[@]}") # Merge 2 arrays
+  # reading filtered rules into an array https://www.computerhope.com/unix/bash/mapfile.htm
+  mapfile -t DHCP_RULES < <( sudo iptables -w -t nat -S | grep MASQUERADE )
 fi
+
+# Same situation applies to VPN connection
+mapfile -t VPN_RULES < <( sudo iptables -w -t nat -S | grep POSTROUTING | grep SNAT )
+
 
 sudo iptables -w -F
 sudo iptables -w -F -t nat
@@ -17,6 +20,12 @@ sudo iptables -w -F -t raw
 sudo ip6tables -w -F
 sudo ip6tables -w -F -t nat
 sudo ip6tables -w -F -t raw
+
+
+for RULE in "${VPN_RULES[@]}";
+do
+  sudo iptables -w -t nat $RULE
+done
 
 if [ "$MODE" = "dhcp" ]
 then
