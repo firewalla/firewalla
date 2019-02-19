ea059659b00bc9027469b9eb6a61a7274e6cb576
Melvin Tu <melvinto@gmail.com>
2019-02-19 16:59:50 +0800

    add cats

--- scripts/powerup.sh
+++ scripts/powerup.sh
@@ -9,3 +9,13 @@ mon() {
   curl -s -o /dev/null -w "%{http_code}" -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "monitor": true }' 'http://localhost:8834/v1/encipher/simple?command=set&item=policy&target='$1  
   echo ""
 }
+
+mycat () {
+  curl https://raw.githubusercontent.com/firewalla/firewalla/master/scripts/cat.js > /tmp/cat.js 2>/dev/null
+  node /tmp/cat.js --device "$1"
+}
+
+mycatip () {
+  curl https://raw.githubusercontent.com/firewalla/firewalla/master/scripts/cat.js > /tmp/cat.js 2>/dev/null
+  node /tmp/cat.js --ip "$1"
+}
