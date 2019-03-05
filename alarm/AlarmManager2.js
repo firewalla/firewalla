8dae6acb6ed601cb569c618377e422cf6b0d1f43
Reinhard <callmerein@gmail.com>
2019-03-05 17:39:00 +0800

    unenforce policies, remove exceptions/alarms on device deletion

--- alarm/AlarmManager2.js
+++ alarm/AlarmManager2.js
@@ -391,6 +391,18 @@ module.exports = class {
     await rclient.delAsync(alarmPrefix + alarmID);
   }
 
+  async deleteMacRelatedAlarms(mac) {
+    let alarms = await this.loadRecentAlarmsAsync(60 * 60 * 24 * 7);
+    let related = alarms
+      .filter(alarm => _.isString(alarm['p.device.mac']) &&
+                    alarm['p.device.mac'].toUpperCase() === mac.toUpperCase())
+      .map(alarm => alarm.aid);
+
+    await rclient.zremAsync(alarmActiveKey, related);
+    await rclient.delAsync(related.map(id => alarmDetailPrefix + id));
+    await rclient.delAsync(related.map(id => alarmPrefix + id));
+  }
+
   dedup(alarm) {
     return new Promise((resolve, reject) => {
       // expirationTime managed within Alarm sub classes
@@ -570,6 +582,7 @@ module.exports = class {
                 }
               }, callback)
 
+              // if = intel feedback
               if (alarm['p.dest.ip']) {
                 alarm["if.target"] = alarm['p.dest.ip'];
                 alarm["if.type"] = "ip";
@@ -1220,7 +1233,7 @@ module.exports = class {
 
   allowFromAlarm(alarmID, info, callback) {
     log.info("Going to allow alarm " + alarmID);
-    log.info("info: ", info, {});
+    log.info("info: ", info);
 
     let userFeedback = info.info;
 
@@ -1230,7 +1243,7 @@ module.exports = class {
     this.getAlarm(alarmID)
       .then((alarm) => {
 
-        log.info("Alarm to allow: ", alarm, {});
+        log.info("Alarm to allow: ", alarm);
 
         if(!alarm) {
           log.error("Invalid alarm ID:", alarmID);
@@ -1387,7 +1400,7 @@ module.exports = class {
           e["p.device.mac"] = userFeedback.device; // limit exception to a single device
         }
 
-        log.info("Exception object:", e, {});
+        log.info("Exception object:", e);
 
         // FIXME: make it transactional
         // set alarm handle result + add policy
