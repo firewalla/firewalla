872d378527036e700ac93109e909cc9b67997b0a
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-05-07 15:30:43 +0800

    ipv6 default on for beta/alpha/dev

--- net2/SpooferManager.js
+++ net2/SpooferManager.js
@@ -98,7 +98,11 @@ module.exports = class SpooferManager {
 
         // feature change listener
         (async () => {
-          if(fc.isFeatureOn("ipv6")) {
+          let ipv6Default = false;
+          if (firewalla.isBeta() || firewalla.isAlpha() || firewalla.isDevelopmentVersion()) {
+            ipv6Default = true;
+          }
+          if(fc.isFeatureOn("ipv6", ipv6Default)) {
             await this.ipv6On();
           } else {
             await this.ipv6Off();
