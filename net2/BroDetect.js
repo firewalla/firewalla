056933e58690fea5e40a6b80e14dd257e4b1c081
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-05-23 18:12:08 +0800

    ignore Firewalla's primary and overlay ip log in BroDetect.js

--- net2/BroDetect.js
+++ net2/BroDetect.js
@@ -540,13 +540,23 @@ module.exports = class {
       return true               // by default, always consider as valid
     }
 
+    const myip = sysManager.myIp();
+    const myip2 = sysManager.myIp2();
+    const myip6 = sysManager.myIp6();
+
     if(m === 'dhcp' || m === 'dhcpSpoof') { // only for dhcp and dhcpSpoof
-      let myip = sysManager.myIp()
-      const myip6 = sysManager.myIp6();
       if (myip) {
         // ignore any traffic originated from walla itself, (walla is acting like router with NAT)
         if (data["id.orig_h"] === myip ||
           data["id.resp_h"] === myip) {
+          return false
+        }
+      }
+
+      if (myip2) {
+        // ignore any traffic originated from walla itself, (walla is acting like router with NAT)
+        if (data["id.orig_h"] === myip2 ||
+          data["id.resp_h"] === myip2) {
           return false
         }
       }
@@ -574,8 +584,6 @@ module.exports = class {
         }
       }
     } else if(m === 'spoof' || m === 'autoSpoof') {
-      let myip = sysManager.myIp()
-      const myip6 = sysManager.myIp6()
       const systemPolicy = hostManager.getPolicyFast();
       const isEnhancedSpoof = (systemPolicy['enhancedSpoof'] == true);
 
@@ -583,6 +591,13 @@ module.exports = class {
       if(myip && 
         (data["id.orig_h"] === myip || data["id.resp_h"] === myip) && 
         (!this.isMonitoring(myip) || isEnhancedSpoof)
+      ) {        
+        return false // set it to invalid if walla itself is set to "monitoring off"
+      }
+
+      if(myip2 && 
+        (data["id.orig_h"] === myip2 || data["id.resp_h"] === myip2) && 
+        (!this.isMonitoring(myip2) || isEnhancedSpoof)
       ) {        
         return false // set it to invalid if walla itself is set to "monitoring off"
       }
@@ -1404,7 +1419,9 @@ module.exports = class {
       // TODO: on DHCP mode, notice could be generated on eth0 or eth0:0 first
       // and the other one will be suppressed. And we'll lost either device/dest info
       if (obj.src != null && obj.src == sysManager.myIp() ||
-          obj.dst != null && obj.dst == sysManager.myIp())
+          obj.src != null && obj.src == sysManager.myIp2() ||
+          obj.dst != null && obj.dst == sysManager.myIp() ||
+          obj.dst != null && obj.dst == sysManager.myIp2())
       {
         return;
       }
