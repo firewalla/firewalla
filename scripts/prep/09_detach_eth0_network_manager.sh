f09dcd8b144e27d3676529d29e76f590cd66241d
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-07-23 11:09:02 +0800

    add comments

--- scripts/prep/09_detach_eth0_network_manager.sh
+++ scripts/prep/09_detach_eth0_network_manager.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [[ -e /etc/NetworkManager/NetworkManager.conf ]]; then
+  # do not use native dnsmasq
   sudo sed -i "s/^dns=dnsmasq/#dns=dnsmasq/" /etc/NetworkManager/NetworkManager.conf
+  # do not manage interfaces in /etc/network/interfaces
   sudo sed -i "s/^managed=true/managed=false/" /etc/NetworkManager/NetworkManager.conf
   sudo systemctl restart NetworkManager
 fi
