2f0da763ce4a0cb43a4be2c1cb1bd3dcbf1c00f3
Edison Liu <jasonlyc.sjtu@gmail.com>
2019-08-05 14:12:33 +0800

    use follow-symlinks while doing sed to update resolv.conf
    Signed-off-by: Edison Liu <jasonlyc.sjtu@gmail.com>

---
+++ scripts/prep/10_ensure_resolvconf_symlink.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RESOLVCONF_PATH=/run/resolvconf/resolv.conf
+
+if [[ -s /etc/resolv.conf && ! -L /etc/resolv.conf ]]; then
+  sudo cp -f /etc/resolv.conf /run/resolvconf/resolv.conf
+  sudo rm /etc/resolv.conf
+  sudo ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
+fi
