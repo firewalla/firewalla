677dca335d31e59bba8b488e5ca6fa9a19a98e34
Reinhard <callmerein@gmail.com>
2019-10-22 17:57:30 +0800

    use log change time as well

--- scripts/brofish-ping.sh
+++ scripts/brofish-ping.sh
@@ -4,16 +4,25 @@
 # In case bro hangs, need to restart it.
 # -----------------------------------------
 
-TOTAL_RETRIES=3
+TOTAL_RETRIES=5
 SLEEP_TIMEOUT=10
 CPU_THRESHOLD=99
 
 # there should be updated logs in log file
 MMIN="-10"
 
-FILE=/dev/shm/bro.touch
+FILE=/blog/current/conn.log
 
 brofish_ping() {
+  RESULT=$(find $FILE -mmin ${MMIN} 2>/dev/null)
+  if [[ -e $FILE && "x$RESULT" == "x" ]]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+brofish_cpu() {
   # Get CPU% from top
   RESULT=$(top -bn1 -p$(cat /blog/current/.pid) |grep bro|awk '{print $9}')
 
@@ -23,6 +32,12 @@ brofish_ping() {
     return 0
   fi
 }
+
+if brofish_ping; then
+  /home/pi/firewalla/scripts/firelog -t cloud -m "brofish ping FAILED, restart brofish now"
+  sudo systemctl restart brofish
+  exit
+fi
 
 retry=1
 ping_ok=0
@@ -36,6 +51,6 @@ while (($retry <= $TOTAL_RETRIES)); do
 done
 
 if [[ $ping_ok -ne 1 ]]; then
-  /home/pi/firewalla/scripts/firelog -t cloud -m "brofish ping FAILED, restart brofish now"
+  /home/pi/firewalla/scripts/firelog -t cloud -m "brofish cpu too high, restart brofish now"
   sudo systemctl restart brofish
 fi
