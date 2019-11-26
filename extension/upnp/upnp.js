206ca2d86d6d546aaf45c3536bcd7e192fb4f7d0
wzjjack <firewalla.zhijie@gmail.com>
2019-11-26 11:34:11 +0800

    update

--- extension/upnp/upnp.js
+++ extension/upnp/upnp.js
@@ -1,4 +1,4 @@
-/*    Copyright 2017 Firewalla INC
+/*    Copyright 2017 - 2019 Firewalla INC
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
