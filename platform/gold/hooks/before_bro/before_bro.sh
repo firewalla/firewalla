5eef3b41a5d075d41c932eb826c0f31165948480
Reinhard <callmerein@gmail.com>
2019-12-24 13:29:09 +0800

    resolve compatibility issues between bro versions

--- platform/gold/hooks/before_bro/before_bro.sh
+++ platform/gold/hooks/before_bro/before_bro.sh
@@ -3,3 +3,6 @@
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 [[ -e $CUR_DIR/broctl.cfg ]] && sudo cp $CUR_DIR/broctl.cfg /usr/local/bro/etc/broctl.cfg
+[[ -e $CUR_DIR/local.bro ]] && sudo cp $CUR_DIR/local.bro /usr/local/bro/share/bro/site/local.bro
+
+export ZEEK_DEFAULT_LISTEN_ADDRESS=127.0.0.1
