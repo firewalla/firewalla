a82d53bc64a4420b8f559e11dcfe1e2379424d1b
Melvin Tu <melvinto@gmail.com>
2020-02-06 02:44:57 +0800

    clean up dnsmasq files when doh is off

--- sensor/DNSCryptPlugin.js
+++ sensor/DNSCryptPlugin.js
@@ -101,8 +101,11 @@ class DNSCryptPlugin extends Sensor {
   }
 
   async applyAll() {
-    await dc.prepareConfig({});
-    await dc.restart();
+    if(this.adminSystemSwitch) {
+      await dc.prepareConfig({});
+      await dc.restart();
+    }
+
     await this.applyDoH();
     for (const macAddress in this.enabledMacAddresses) {
       await this.applyDeviceDoH(macAddress);
@@ -172,7 +175,7 @@ class DNSCryptPlugin extends Sensor {
 
   async globalOff() {
     this.adminSystemSwitch = false;
-    //await this.applyAll();
+    await this.applyAll();
     await dc.stop();
   }
 
