50ec22359b17d85095fd681b8234bb232d352d70
Melvin Tu <melvinto@gmail.com>
2020-02-21 00:25:05 +0800

    bug fix

--- platform/blue/hooks/before_main_run/02_install_libcurl.sh
+++ platform/blue/hooks/before_main_run/02_install_libcurl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 test -e /usr/lib/aarch64-linux-gnu/libcurl.so.4 && exit 0
 
-sudo cp ${FW_PLATFORM_CUR_DIR}/files/libcurl.so.4 /usr/lib/aarch64-linux-gnu/
+sudo cp ${CUR_DIR}/../../files/libcurl.so.4 /usr/lib/aarch64-linux-gnu/
--- platform/red/hooks/before_main_run/02_install_libcurl.sh
+++ platform/red/hooks/before_main_run/02_install_libcurl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 test -e /usr/lib/arm-linux-gnueabihf/libcurl.so.4 && exit 0
 
-sudo cp ${FW_PLATFORM_CUR_DIR}/files/libcurl.so.4 /usr/lib/arm-linux-gnueabihf/
+sudo cp ${CUR_DIR}/../../files/libcurl.so.4 /usr/lib/arm-linux-gnueabihf/
