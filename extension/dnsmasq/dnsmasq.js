3c947a275aa2b08d0b78944185219fd8e65f981b
Melvin Tu <melvinto@gmail.com>
2020-02-22 14:29:33 +0800

    add dnsmasq.sh and dnsmasq.conf to check list

--- extension/dnsmasq/dnsmasq.js
+++ extension/dnsmasq/dnsmasq.js
@@ -1534,7 +1534,7 @@ module.exports = class DNSMASQ {
     try {
       const dnsmasqConfKey = "dnsmasq:conf";
       let md5sumNow = '';
-      for (const confs of [`${FILTER_DIR}*`, resolvFile]) {
+      for (const confs of [`${FILTER_DIR}*`, resolvFile, startScriptFile, configFile]) {
         const { stdout } = await execAsync(`find ${confs} -type f | sort | xargs cat | md5sum | awk '{print $1}'`);
         md5sumNow = md5sumNow + (stdout ? stdout.split('\n').join('') : '');
       }
