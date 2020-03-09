c05a5e4586e7362d21d31fc42955d7189d00507c
Melvin Tu <melvinto@gmail.com>
2020-03-10 01:52:49 +0800

    bug fix on blog

--- platform/gold/hooks/before_bro/before_bro.sh
+++ platform/gold/hooks/before_bro/before_bro.sh
@@ -4,6 +4,10 @@ CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 [[ -e $CUR_DIR/broctl.cfg ]] && sudo cp $CUR_DIR/broctl.cfg /usr/local/bro/etc/broctl.cfg
 
+if [[ -e /log/blog ]]; then
+  sudo ln -s /log/blog/current /blog/current
+fi
+
 TMP_FILE="/home/pi/.firewalla/config/local.bro"
 if [ -f "${TMP_FILE}" ]; then
   [[ -e $CUR_DIR/local.bro ]] && sudo bash -c "cat $CUR_DIR/local.bro ${TMP_FILE} > /usr/local/bro/share/bro/site/local.bro"
