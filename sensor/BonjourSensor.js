39ce781d0d21d3af34d4ea1e35c808732676ed9c
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-03-31 11:02:25 +0800

    create separate bonjour instance for each monitoring network

--- sensor/BonjourSensor.js
+++ sensor/BonjourSensor.js
@@ -41,8 +41,7 @@ const ipMacCache = {};
 class BonjourSensor extends Sensor {
   constructor() {
     super();
-    this.interfaces = null;
-    this.hostCache = {};
+    this.bonjourListeners = [];
   }
 
   scheduleReload() {
@@ -54,57 +53,53 @@ class BonjourSensor extends Sensor {
         clearTimeout(this.startTask);
       if (this.updateTask)
         clearInterval(this.updateTask);
-      if (this.bonjourBrowserTCP)
-        this.bonjourBrowserTCP.stop();
-      if (this.bonjourBrowserUDP)
-        this.bonjourBrowserUDP.stop();
-      if (this.bonjourBrowserHTTP)
-        this.bonjourBrowserHTTP.stop();
-      if (this.bonjour)
-        this.bonjour.destroy();
+      
+      for (const listener of this.bonjourListeners) {
+        if (listener.tcpBrowser)
+          listener.tcpBrowser.stop();
+        if (listener.udpBrowser)
+          listener.udpBrowser.stop();
+        if (listener.httpBrowser)
+          listener.httpBrowser.stop();
+        if (listener.instance)
+          listener.instance.destroy();
+      }
+      this.bonjourListeners = [];
+
       // do not initialize bonjour if there is no interface with IP address
       // otherwise dgram.addMembership will emit error and crash the process
       if (sysManager.getMonitoringInterfaces().filter(i => i.ip_address).length == 0)
         return;
       // create new bonjour listeners
-      this.bonjour = Bonjour();
-      this.bonjour._server.mdns.on('warning', (err) => log.warn("Warning on mdns server", err));
-      this.bonjour._server.mdns.on('error', (err) => log.error("Error on mdns server", err));
-      this.interfaces = sysManager.getMonitoringInterfaces();
-      this.bonjourBrowserTCP = this.bonjour.find({
-        protocol: 'tcp'
-      }, (service) => {
-        this.bonjourParse(service);
-      });
-      this.bonjourBrowserUDP = this.bonjour.find({
-        protocol: 'udp'
-      }, (service) => {
-        this.bonjourParse(service);
-      });
-      // why http?? because sometime http service can't be found via { protocol: 'tcp' }
-      // maybe it's bonjour lib's bug
-      this.bonjourBrowserHTTP = this.bonjour.find({
-        type: 'http'
-      }, (service) => {
-        this.bonjourParse(service);
-      });
-
+      for (const iface of sysManager.getMonitoringInterfaces().filter(i => i.ip_address)) {
+        const instance = Bonjour({interface: iface.ip_address});
+        instance._server.mdns.on('warning', (err) => log.warn(`Warning from mDNS server on ${iface.ip_address}`, err));
+        instance._server.mdns.on('error', (err) => log.error(`Error from mDNS server on ${iface.ip_address}`, err));
+        const tcpBrowser = instance.find({protocol: 'tcp'}, (service) => this.bonjourParse(service));
+        const udpBrowser = instance.find({protocol: 'udp'}, (service) => this.bonjourParse(service));
+        const httpBrowser = instance.find({type: 'http'}, (service) => this.bonjourParse(service));
+        this.bonjourListeners.push({tcpBrowser, udpBrowser, httpBrowser, instance});
+      }
 
       this.updateTask = setInterval(() => {
         log.info("Bonjour Watch Updating");
         // remove all detected servcies in bonjour browser internally, otherwise BonjourBrowser would do dedup based on service name, and ip changes would be ignored
-        Object.keys(this.bonjourBrowserTCP._serviceMap).forEach(fqdn => this.bonjourBrowserTCP._removeService(fqdn));
-        Object.keys(this.bonjourBrowserUDP._serviceMap).forEach(fqdn => this.bonjourBrowserUDP._removeService(fqdn));
-        Object.keys(this.bonjourBrowserHTTP._serviceMap).forEach(fqdn => this.bonjourBrowserHTTP._removeService(fqdn));
-        this.bonjourBrowserTCP.update();
-        this.bonjourBrowserUDP.update();
-        this.bonjourBrowserHTTP.update();
+        for (const listener of this.bonjourListeners) {
+          Object.keys(listener.tcpBrowser._serviceMap).forEach(fqdn => listener.tcpBrowser._removeService(fqdn));
+          Object.keys(listener.udpBrowser._serviceMap).forEach(fqdn => listener.udpBrowser._removeService(fqdn));
+          Object.keys(listener.httpBrowser._serviceMap).forEach(fqdn => listener.httpBrowser._removeService(fqdn));
+          listener.tcpBrowser.update();
+          listener.udpBrowser.update();
+          listener.httpBrowser.update();
+        }
       }, 1000 * 60 * 5);
   
       this.startTask = setTimeout(() => {
-        this.bonjourBrowserTCP.start();
-        this.bonjourBrowserUDP.start();
-        this.bonjourBrowserHTTP.start();
+        for (const listener of this.bonjourListeners) {
+          listener.tcpBrowser.start();
+          listener.udpBrowser.start();
+          listener.httpBrowser.start();
+        }
       }, 1000 * 10);
     }, 5000);
   }
@@ -120,26 +115,6 @@ class BonjourSensor extends Sensor {
       }
     });
     sclient.subscribe(Message.MSG_SYS_NETWORK_INFO_RELOADED);
-  }
-
-  // do not process same host in a short time
-  isDup(service) {
-    let key = service.ipv4Addr;
-    if (!key) {
-      return true;
-    }
-
-    if (this.hostCache[key]) {
-      log.debug("Ignoring duplicated bonjour services from same ip:", key);
-      return true;
-    }
-
-    this.hostCache[key] = 1;
-    setTimeout(() => {
-      delete this.hostCache[key];
-    }, 5 * 1000 * 60); // 5 mins
-
-    return false;
   }
 
   async _getMacFromIP(ipAddr) {
@@ -294,10 +269,7 @@ class BonjourSensor extends Sensor {
       host: service.host
     };
 
-    // do not dedup since it is only run once every 5 minutes
-    //if(!this.isDup(s)) {
     this.processService(s);
-    //}
   }
 }
 
