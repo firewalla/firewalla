daa6a62cf62419bee051645b736ebf5083449245
child <firewalla.zhijie@gmail.com>
2020-04-21 16:48:56 +0800

    update

--- control/BlockManager.js
+++ control/BlockManager.js
@@ -114,7 +114,7 @@ class BlockManager {
                 // if the ip shared with other domain, should not apply ip level block
                 // if a.com and b.com share ip and one of them block, it should be domain level
                 // if both block, should update to ip level
-                ipBlockInfo.targetDomains.push(domain);
+                !ipBlockInfo.targetDomains.include(domain) && ipBlockInfo.targetDomains.push(domain);
                 const allDomains = await dnsTool.getAllDns(ip);
                 const sharedDomains = _.differenceWith(allDomains, ipBlockInfo.targetDomains, (a, b) => {
                     return this.domainCovered(b, a);
@@ -132,7 +132,7 @@ class BlockManager {
                 break;
             }
             case 'unblock': {
-                ipBlockInfo.sharedDomains.push(domain);
+                !ipBlockInfo.sharedDomains.include(domain) && ipBlockInfo.sharedDomains.push(domain);
                 ipBlockInfo.targetDomains = _.filter(ipBlockInfo.targetDomains, (a) => {
                     return a != domain;
                 })
