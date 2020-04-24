5384a6612b4187631b25a57197236b66310ffb43
Melvin Tu <melvinto@gmail.com>
2020-04-24 15:40:25 +0800

    update alias

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -82,15 +82,12 @@ alias ggalpha='cd /home/firewalla; scripts/switch_branch.sh beta_7_0 && /home/pi
 function ggsupport {
   SUPPORT_TOKEN=$1
   PORT=$2
-  SERVER=$3
-
-  if [[ -z $SERVER ]]; then
-    SERVER="support.firewalla.com"
-  fi
+  SERVER_PORT=${3:-10000}
+  SERVER=${4:-support.firewalla.com}
 
 echo "[common]
 server_addr = $SERVER
-server_port = 10000
+server_port = $SERVER_PORT
 privilege_token = $SUPPORT_TOKEN
 
 [SSH$PORT]
