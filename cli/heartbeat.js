a7b60989ef37cc4ea31805c4a3db96d450a3bc35
Melvin Tu <melvinto@gmail.com>
2020-06-01 12:25:05 +0800

    update hb

--- cli/heartbeat.js
+++ cli/heartbeat.js
@@ -19,6 +19,10 @@ function getSignatureMac() {
 
 const socket = io2(url, { path: path, transports: ['websocket'], 'upgrade': false });
 
+function log(message) {
+  console.log(new Date(), message);
+}
+
 function getSysinfo(status) {
   return {mac, status};
 }
@@ -33,18 +37,19 @@ const job = setTimeout(() => {
 }, 30 * 3600 * 1000);
 
 socket.on('connect', () => {
-  console.log(new Date(), "Connected to heartbeat server.");
+  log("Connected to heartbeat server.");
   update('connect');
 });
 
 socket.on('disconnect', () => {
-  console.log(new Date(), "Disconnected from heartbeat server.");
+  log("Disconnected from heartbeat server.");
 });
 
 socket.on("update", (data) => {
+  update("cloud");
 });
 
 socket.on('reconnect', () => {
-  console.log(new Date(), "Reconnected to heartbeat server.");
+  log("Reconnected to heartbeat server.");
   update('reconnect');
 });
