7d609514b83f5e637f219c780e9471f305153442
Reinhard <callmerein@gmail.com>
2020-06-18 17:04:49 +0800

    remove bro_proc_name function

--- platform/platform.sh
+++ platform/platform.sh
@@ -4,11 +4,9 @@ FW_PLATFORM_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 UNAME=$(uname -m)
 
-GOLD=gold
-
 case "$UNAME" in
   "x86_64")
-    source $FW_PLATFORM_DIR/$GOLD/platform.sh
+    source $FW_PLATFORM_DIR/gold/platform.sh
     FW_PLATFORM_CUR_DIR=$FW_PLATFORM_DIR/gold
     BRO_PROC_NAME="zeek"
     BRO_PROC_COUNT=6
@@ -49,8 +47,4 @@ function before_bro {
       $script
     done
   fi
-}
-
-function bro_proc_name {
-  echo $BRO_PROC_NAME
 }
