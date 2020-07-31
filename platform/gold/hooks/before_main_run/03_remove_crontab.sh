70241f6901a0d4fdf3efd4d15719c348cfe0c713
Melvin Tu <melvinto@gmail.com>
2020-07-31 15:35:19 +0800

    add timeout

--- platform/gold/hooks/before_main_run/03_remove_crontab.sh
+++ platform/gold/hooks/before_main_run/03_remove_crontab.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo bash -c "test -e /media/root-ro/var/spool/cron/crontabs/pi && overlayroot-chroot /bin/rm /var/spool/cron/crontabs/pi &>/dev/null"
+# add timeout to make sure the command exit within 5 seconds
+sudo timeout 5 bash -c "test -e /media/root-ro/var/spool/cron/crontabs/pi && overlayroot-chroot /bin/rm /var/spool/cron/crontabs/pi &>/dev/null"
