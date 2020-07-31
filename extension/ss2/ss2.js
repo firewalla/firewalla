4796143b78508d9f8c122b481f0a1c356eb0caa0
Melvin Tu <melvinto@gmail.com>
2020-07-31 18:30:31 +0800

    bug fix on ss2

--- extension/ss2/ss2.js
+++ extension/ss2/ss2.js
@@ -125,6 +125,15 @@ class SS2 {
 
       await exec(`FW_SS_SERVER=${this.config.server} FW_SS_REDIR_PORT=9954 NAME=${this.getChainName()} ${__dirname}/setup_iptables.sh`);
 
+      if(_.isArray(this.config.excludes)) {
+        for(const exclude of this.config.excludes) {
+          const result = exec(`dig +short ${exclude}`);
+          if(!_.isEmpty(result && result.stdout)) {
+            await exec(`sudo ipset add fw_ss2_whitelist ${result.stdout} &>/dev/null`);
+          }
+        }
+      }
+
       this.ready = true;
     } catch (err) {
       log.error("Got error when reading preparing config, err:", err);
