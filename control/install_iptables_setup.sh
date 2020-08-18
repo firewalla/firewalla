078bf40bb32f148f1cc945f2fd3d489f44448f52
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-08-18 17:38:34 +0800

    add forward compatibility with 1.971

--- control/install_iptables_setup.sh
+++ control/install_iptables_setup.sh
@@ -825,3 +825,24 @@ if [[ $(uname -m) == "x86_64" ]]; then
   sudo iptables -w -A DOCKER-USER -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
   sudo iptables -w -A DOCKER-USER -j RETURN
 fi
+
+if ip link show dev ifb0; then
+  sudo tc filter delete dev ifb0 &> /dev/null || true
+  sudo tc qdisc delete dev ifb0 root &> /dev/null || true
+  sudo ip link set ifb0 up
+  sudo tc filter del dev ifb0
+  sudo tc qdisc replace dev ifb0 root handle 1: htb default 1
+  # 4 is the default priority
+  sudo tc class add dev ifb0 parent 1: classid 1:1 htb rate 3072mbit prio 4
+  sudo tc qdisc replace dev ifb0 parent 1:1 fq
+fi
+
+if ip link show dev ifb1; then
+  sudo tc filter delete dev ifb1 &> /dev/null || true
+  sudo tc qdisc delete dev ifb1 root &> /dev/null || true
+  sudo ip link set ifb1 up
+  sudo tc filter del dev ifb1
+  sudo tc qdisc replace dev ifb1 root handle 1: htb default 1
+  sudo tc class add dev ifb1 parent 1: classid 1:1 htb rate 3072mbit prio 4
+  sudo tc qdisc replace dev ifb1 parent 1:1 fq
+fi
