d34905f1bcb6d2674bbd6f97101d87a1823fbc8f
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-08-18 16:58:42 +0800

    do not allow parallel execution of NetworkProfileManager.refreshNetworkProfiles

--- net2/NetworkProfileManager.js
+++ net2/NetworkProfileManager.js
@@ -79,13 +79,25 @@ class NetworkProfileManager {
     if (this.refreshTask)
       clearTimeout(this.refreshTask);
     this.refreshTask = setTimeout(async () => {
-      await this.refreshNetworkProfiles();
-      if (f.isMain()) {
-        if (this.iptablesReady) {
-          for (let uuid in this.networkProfiles) {
-            const networkProfile = this.networkProfiles[uuid];
-            networkProfile.scheduleApplyPolicy();
+      if (this._refreshInProgress) {
+        log.info("Refresh network profiles in progress, will schedule later ...");
+        this.scheduleRefresh();
+      } else {
+        try {
+          this._refreshInProgress = true;
+          await this.refreshNetworkProfiles();
+          if (f.isMain()) {
+            if (this.iptablesReady) {
+              for (let uuid in this.networkProfiles) {
+                const networkProfile = this.networkProfiles[uuid];
+                networkProfile.scheduleApplyPolicy();
+              }
+            }
           }
+        } catch (err) {
+          log.error("Failed to refresh network profiles", err);
+        } finally {
+          this._refreshInProgress = false;
         }
       }
     }, 3000);
