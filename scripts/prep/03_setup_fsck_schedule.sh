64c23dd4dfa988adbf01f224fe9390f87cd85e0e
Reinhard Liou <callmerein@gmail.com>
2020-11-09 17:55:21 +0800

    use blkid instead of file

--- scripts/prep/03_setup_fsck_schedule.sh
+++ scripts/prep/03_setup_fsck_schedule.sh
@@ -5,6 +5,6 @@ INTERVAL=4w
 
 if ls -1 /dev/mmcblk0p* &> /dev/null; then
     for device in `ls -1 /dev/mmcblk0p*`; do
-        (sudo file -sL $device | grep "Linux rev 1.0 ext4" &> /dev/null) && sudo tune2fs -c $MOUNT_COUNT -i $INTERVAL $device > /dev/null
+        (sudo blkid -o value -s TYPE $device | grep "ext4" &> /dev/null) && sudo tune2fs -c $MOUNT_COUNT -i $INTERVAL $device > /dev/null
     done
 fi
