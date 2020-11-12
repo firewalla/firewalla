b6e2f48755699b8596f8beb19ff7f01817599af9
Jian Yang <pub@yangjian.net>
2020-11-12 19:07:16 +0800

    make run_horse_light configurable

--- platform/gold/platform.sh
+++ platform/gold/platform.sh
@@ -64,10 +64,12 @@ function map_target_branch {
 }
 
 function run_horse_light {
+  flash_interval=${1:-2}
+  pause_interval=${2:-1}
   sudo pkill -9 ethtool
   for ((i=3;i>=0;i--))
   do
-    sudo timeout 2 ethtool -p eth${i}
-    sleep 1
+    sudo timeout $flash_interval ethtool -p eth${i}
+    sleep $pause_interval
   done
 }
