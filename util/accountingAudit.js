cbd8f5132a370ecc6c223c9520e38edaa210aae2
Melvin Tu <melvinto@gmail.com>
2020-11-17 10:21:10 +0000

    update copyright

--- util/accountingAudit.js
+++ util/accountingAudit.js
@@ -1,6 +1,6 @@
 'use strict'
 
-/*    Copyright 2016 Firewalla LLC 
+/*    Copyright 2020 Firewalla Inc.
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
