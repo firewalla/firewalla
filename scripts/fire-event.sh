39cdc1e38eb5415f6b6250d0c1f4710cd6e8b32a
Jian Yang <pub@yangjian.net>
2020-11-25 19:09:24 +0800

    provide CLI for events

---
+++ scripts/fire-event.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# ----------------------------------------------------------------------------
+# Constants
+# ----------------------------------------------------------------------------
+
+CMD=$(basename $0)
+CMDDIR=$(dirname $0)
+FIREWALLA_HOME=$(cd $CMDDIR; git rev-parse --show-toplevel)
+: ${FIREWALLA_HOME:=/home/pi/firewalla}
+source ${FIREWALLA_HOME}/platform/platform.sh
+
+# ----------------------------------------------------------------------------
+# Function
+# ----------------------------------------------------------------------------
+
+usage() {
+    cat <<EOU
+usage: $CMD {get|state|action|del} [<options>]
+
+  # get events from <begin> till <end>
+  $CMD get <begin> <end>
+
+  # delete events from <begin> till <end>
+  $CMD del <begin> <end>
+
+  # add state event with state_type/state_key/state_value
+  $CMD state <state_type> <state_key> <state_value>
+
+  # add action event with action_type/action_value
+  $CMD action <action_type> <action_value>
+
+EOU
+}
+
+mylog() {
+    echo "$(date):$@"
+}
+
+loginfo() {
+    mylog "INFO: $@"
+}
+
+logerror() {
+    mylog "ERROR:$@" >&2
+}
+
+add_state_event() {
+    test $# -ge 3 || { logerror need state_type/state_key/state_value; return 1; }
+    redis-cli publish 'event:request:state' "{\"state_type\":\"$1\",\"state_key\":\"$2\",\"state_value\":$3}"
+}
+
+add_action_event() {
+    test $# -ge 3 || { logerror need action_type/action_value; return 1; }
+    redis-cli publish 'event:request:action' "{\"action_type\":\"$1\",\"action_value\":$2}"
+}
+
+del_event() {
+    begin=${1:-0}
+    end=${2:-0}
+    redis-cli publish 'event:request:clean' "{\"begin\":\"$begin\",\"end\":"$end"}"
+}
+
+get_event() {
+    redis-cli zrangebyscore 'event:log' ${1:-'0'} ${2:-'inf'} withscores
+}
+
+# ----------------------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------------------
+
+rc=0
+
+test $# -gt 0 || { usage; exit 0; }
+
+cmd=$1; shift
+
+case $cmd in 
+    get) get_event "$@" || rc=1
+        ;;
+    state) add_state_event "$@" || rc=1
+        ;;
+    action) add_action_event "$@" || rc=1
+        ;;
+    del) del_event "$@" || rc=1
+        ;;
+    *) usage
+       logerror "un-supported command - $cmd"
+       rc=1
+        ;;
+esac
+
+exit $rc
