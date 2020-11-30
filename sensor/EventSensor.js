4a74693498a697535827baa0a6dbdeaa7da7ae60
Jian Yang <pub@yangjian.net>
2020-11-30 17:32:43 +0800

    update copyright

--- sensor/EventSensor.js
+++ sensor/EventSensor.js
@@ -1,4 +1,4 @@
-/*    Copyright 2018 Firewalla LLC
+/*    Copyright 2020 Firewalla LLC
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
