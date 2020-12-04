68ef02b33a379ef631e6ff728c68bc89c1d8f354
Reinhard L <callmerein@gmail.com>
2020-12-04 15:44:02 +0800

    reduce ts precision to save memory

--- net2/BroDetect.js
+++ net2/BroDetect.js
@@ -834,6 +834,10 @@ module.exports = class {
       }
       */
 
+      // keep only 2 digits after decimal to save memory
+      obj.ts = Math.round(obj.ts * 100) / 100
+      obj.duration = Math.round(obj.duration * 100) / 100
+
       // Long connection aggregation
       const uid = obj.uid
       if (long || this.activeLongConns[uid]) {
@@ -1048,7 +1052,7 @@ module.exports = class {
         af: {}, //application flows
         pr: obj.proto,
         f: flag,
-        flows: [flowDescriptor],
+        flows: [flowDescriptor], // TODO: deprecate this to save memory
         uids: [obj.uid]
       };
 
