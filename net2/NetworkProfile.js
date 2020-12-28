e3ea790ec8c7f8b704106b7bc0a2248e34d60c57
Edison Liu <jasonlyc.sjtu@gmail.com>
2020-12-28 14:37:52 +0800

    use a better way to add ip rule for vpn client in DHCP mode

--- net2/NetworkProfile.js
+++ net2/NetworkProfile.js
@@ -448,12 +448,6 @@ class NetworkProfile {
           } else {
             await exec(rule.toCmd('-D')).catch((err) => {});
           }
-          // add subnet to wan_routable table in case it is actually a WAN in DHCP mode, which should be considered as LAN
-          if (this.o.type === "lan" && this.o.gateway && await Mode.isDHCPModeOn()) {
-            const subnet4 = iptool.cidrSubnet(subnet);
-            const prefix = `${subnet4.networkAddress}/${subnet4.subnetMaskLength}`;
-            await routing.addRouteToTable(prefix, null, realIntf, "wan_routable", 100, 4).catch((err) => {});
-          }
         }
       }
       // add to monitored net ipset accordingly
@@ -537,10 +531,6 @@ class NetworkProfile {
         for (const subnet of this.o.ipv4Subnets) {
           const rule = new Rule("nat").chn("FW_POSTROUTING_HAIRPIN").mth(`${subnet}`, null, "src").jmp("MASQUERADE");
           await exec(rule.toCmd('-D')).catch((err) => {});
-          // remove subnet from wan_routable table as a counter-part of createEnv
-          const subnet4 = iptool.cidrSubnet(subnet);
-          const prefix = `${subnet4.networkAddress}/${subnet4.subnetMaskLength}`;
-          await routing.removeRouteFromTable(prefix, null, realIntf, "wan_routable", 100, 4).catch((err) => {});
         }
       }
       // still remove it from monitored net set anyway to keep consistency
