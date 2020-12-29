ef98b3dd761876b6754376bcbd40964cdffc20d6
Jian Yang <pub@yangjian.net>
2020-12-29 19:43:23 +0800

    add stat calculation

--- sensor/NetworkMonitorSensor.js
+++ sensor/NetworkMonitorSensor.js
@@ -41,7 +41,8 @@ class NetworkMonitorSensor extends Sensor {
 
   constructor() {
     super()
-    this.scheduledJobs = {};
+    this.sampleJobs = {};
+    this.statJobs = {};
     this.savedDevices = {};
   }
 
@@ -49,13 +50,6 @@ class NetworkMonitorSensor extends Sensor {
   Default config in config.json might have following supported PLACEHOLDERS
   ---------------------------------------------------------------------------
     "NetworkMonitorSensor": {
-      "stat": {
-        "calculateInterval":  300
-      },
-      "cleanup": {
-        "expirePeriod": 86400
-      },
-      "targets": {
         "MY_GATEWAYS": {
           "ping": {
               "sampleCount": 20,
@@ -63,7 +57,6 @@ class NetworkMonitorSensor extends Sensor {
           }
         },
         ...
-      }
     }
   ----------------------------------------------------------------------------
    */
@@ -71,7 +64,7 @@ class NetworkMonitorSensor extends Sensor {
     let defaultConfig = {};
     if (this.config) {
       try {
-        const cfg = this.config.targets
+        const cfg = this.config
         log.info("Loading default network monitor config ...");
         Object.keys(cfg).forEach ( key => {
           switch (key) {
@@ -90,8 +83,8 @@ class NetworkMonitorSensor extends Sensor {
               break;
           }
         });
-        log.info("this.config: ", JSON.stringify(this.config,null,4));
-        log.info("defaultConfig: ", JSON.stringify(defaultConfig,null,4));
+        log.debug("this.config: ", JSON.stringify(this.config,null,4));
+        log.debug("defaultConfig: ", JSON.stringify(defaultConfig,null,4));
       } catch(err) {
         log.error("Failed to load default network monitor config: ", err);
       }
@@ -101,28 +94,10 @@ class NetworkMonitorSensor extends Sensor {
 
   async run() {
     
-    /*
-     * global feature switch(on/off) with static configuration(config.json)
-     */
-
     log.info("run NetworkMonitorSensor ...");
-    if (fc.isFeatureOn(FEATURE_NETWORK_MONITOR)) {
-      await this.turnOn();
-    } else {
-      await this.turnOff();
-    }
-
-    fc.onFeature(FEATURE_NETWORK_MONITOR, (feature, status) => {
-      if (feature != FEATURE_NETWORK_MONITOR) return;
-      if (status) {
-        this.turnOn();
-      } else {
-        this.turnOff();
-      }
-    })
 
     /*
-     * feature switches in policy
+     * apply policy upon policy change or startup
      */
     extensionManager.registerExtension(POLICY_KEYNAME, this, {
       applyPolicy: this.applyPolicy,
@@ -141,6 +116,8 @@ class NetworkMonitorSensor extends Sensor {
           const runtimeConfig = systemConfig || this.loadDefaultConfig();
           log.info("runtimeConfig: ",runtimeConfig);
           Object.keys(runtimeConfig).forEach( async targetIP => {
+            // always restart to run with latest config
+            this.stopMonitorDevice(targetIP);
             await this.startMonitorDevice(targetIP, targetIP, runtimeConfig[targetIP]);
           });
           break;
@@ -160,12 +137,12 @@ class NetworkMonitorSensor extends Sensor {
   }
 
   async samplePing(target, cfg) {
-    //log.info(`sample PING to ${target} with cfg(${JSON.stringify(cfg,null,4)})`);
+    log.info(`sample PING to ${target} with cfg(${JSON.stringify(cfg,null,4)})`);
     try {
       const timeNow = Date.now();
       const timeSlot = (timeNow - timeNow % (cfg.sampleInterval*1000))/1000;
-      const result = await exec(`ping -c ${cfg.sampleCount} -4 -n -w 3 ${target}| awk '/time=/ {print $7}' | cut -d= -f2`)
-      const data = result.stdout.trim().split(/\n/);
+      const result = await exec(`ping -c ${cfg.sampleCount} -4 -n ${target}| awk '/time=/ {print $7}' | cut -d= -f2`)
+      const data = result.stdout.trim().split(/\n/).map(e => parseFloat(e));
       this.recordSampleDataInRedis(MONITOR_PING, target, timeSlot, data);
     } catch (err) {
       log.error("failed to sample PING:",err);
@@ -180,7 +157,7 @@ class NetworkMonitorSensor extends Sensor {
       let data = [];
       for (let i=0;i<cfg.sampleCount;i++) {
         const result = await exec(`dig @${target} ${cfg.lookupName} | awk '/Query time:/ {print $4}'`);
-        data.push(result.stdout.trim());
+        data.push(parseInt(result.stdout.trim()));
       }
       //this.recordSampleDataInRedis(MONITOR_DNS, `${target}:${cfg.lookupName}`, timeSlot, data);
       this.recordSampleDataInRedis(MONITOR_DNS, target, timeSlot, data);
@@ -197,7 +174,7 @@ class NetworkMonitorSensor extends Sensor {
       let data = [];
       for (let i=0;i<cfg.sampleCount;i++) {
         const result = await exec(`curl -m 10 -w '%{time_total}\n' '${target}'`);
-        data.push(result.stdout.trim());
+        data.push(parseFloat(result.stdout.trim()));
       }
       this.recordSampleDataInRedis(MONITOR_HTTP, target, timeSlot, data);
     } catch (err) {
@@ -239,11 +216,13 @@ class NetworkMonitorSensor extends Sensor {
     log.info(`start monitoring ${key} with ip(${ip}) and cfg(${JSON.stringify(cfg,null,4)}) ...`)
     for ( const monitorType of Object.keys(cfg) ) {
       const scheduledKey = `${key}-${monitorType}`;
-      if ( scheduledKey in this.scheduledJobs ) {
+      if ( scheduledKey in this.sampleJobs ) {
         log.warn(`${monitorType} on ${key} already started`);
       } else {
-        log.info(`scheduling ${monitorType} on ${key} ...`);
-        this.scheduledJobs[scheduledKey] = this.scheduleSampleJob(monitorType,ip,cfg[monitorType]);
+        log.debug(`scheduling sample job ${monitorType} on ${key} ...`);
+        this.sampleJobs[scheduledKey] = this.scheduleSampleJob(monitorType,ip,cfg[monitorType]);
+        log.debug(`scheduling stat job ${monitorType} on ${key} ...`);
+        this.statJobs[scheduledKey] = this.scheduleStatJob(monitorType,ip,cfg[monitorType]);
       }
     }
   }
@@ -252,10 +231,17 @@ class NetworkMonitorSensor extends Sensor {
     log.info(`stop monitoring ${key} ...`)
     for ( const monitorType of MONITOR_TYPES ) {
       const scheduledKey = `${key}-${monitorType}`;
-      if ( scheduledKey in this.scheduledJobs ) {
-        log.info(`UNscheduling ${monitorType} on ${key} ...`);
-        clearInterval(this.scheduledJobs[scheduledKey]);
-        delete(this.scheduledJobs[scheduledKey])
+      if ( scheduledKey in this.sampleJobs ) {
+        log.debug(`UNscheduling ${monitorType} on ${key} ...`);
+        clearInterval(this.sampleJobs[scheduledKey]);
+        delete(this.sampleJobs[scheduledKey])
+      } else {
+        log.warn(`${monitorType} on ${key} NOT scheduled`);
+      }
+      if ( scheduledKey in this.statJobs ) {
+        log.debug(`UNscheduling ${monitorType} on ${key} ...`);
+        clearInterval(this.statJobs[scheduledKey]);
+        delete(this.statJobs[scheduledKey])
       } else {
         log.warn(`${monitorType} on ${key} NOT scheduled`);
       }
@@ -263,12 +249,17 @@ class NetworkMonitorSensor extends Sensor {
   }
 
   stopMonitorDeviceAll() {
-    Object.keys(this.scheduledJobs).forEach( scheduledKey => {
-      log.info(`UNscheduling ${scheudledKey} ...`);
-      clearInterval(this.scheduledJobs[scheduledKey]);
-      delete(this.scheduledJobs[scheduledKey]);
+    Object.keys(this.sampleJobs).forEach( scheduledKey => {
+      log.debug(`UNscheduling ${scheudledKey} in sample jobs ...`);
+      clearInterval(this.sampleJobs[scheduledKey]);
+      delete(this.sampleJobs[scheduledKey]);
     })
-    log.info("after stop all: ", JSON.stringify(this.scheduledJobs,null,4));
+    Object.keys(this.statJobs).forEach( scheduledKey => {
+      log.debug(`UNscheduling ${scheudledKey} in stat jobs ...`);
+      clearInterval(this.statJobs[scheduledKey]);
+      delete(this.statJobs[scheduledKey]);
+    })
+    log.info("after stop all: ", JSON.stringify(this.sampleJobs,null,4));
   }
 
   async applyPolicyDevice(host, state, cfg) {
@@ -277,7 +268,9 @@ class NetworkMonitorSensor extends Sensor {
       const key = host.o.mac;
       switch ( state ) {
         case true: {
-          this.startMonitorDevice(key, host.o.ip, cfg);
+          // always restart to run with latest config
+          this.stopMonitorDevice(key);
+          this.startMonitorDevice(key, host.o.ipv4Addr, cfg);
           break;
         }
         case false: {
@@ -317,34 +310,11 @@ class NetworkMonitorSensor extends Sensor {
     }
   }
 
-  async turnOn() {
-    try {
-      log.info("Feature is turned on.");
-      // system level : load from policy or start by default policy
-      const policyConfigJSON = await rclient.hgetAsync('policy:system', POLICY_KEYNAME )
-      const policyConfig = policyConfigJSON ? JSON.parse(policyConfigJSON) : { "state":true, "config": null};
-      log.info("policyConfig:",policyConfig);
-      this.applyPolicySystem(policyConfig.state,policyConfig.config);
-      // device level
-      for (const mac in Object.keys(this.savedDevices)) {
-        const policyDevice = await rclient.hgetAsync('policy:mac', mac);
-        await this.applyPolicyDevice(this.savedDevices[mac], policyDevice.state, policyDevice.config);
-      }
-    } catch (err) {
-      log.error(`failed to turn on feature ${FEATURE_NETWORK_MONITOR}: `,err)
-    }
-  }
-
-  async turnOff() {
-    this.stopMonitorDeviceAll();
-    log.info("Feature is turned off.");
-  }
-
   async getNetworkMonitorData() {
     try {
       for (const monitorType of MONITOR_TYPES) {
         const redisKey = `${KEY_PREFIX_RAW}:${monitorType}`
-        const config_json = await rclient.hgetAsync(redisKey);
+        const config_json = rclient.hgetall(redisKey);
         result[$monitorType] = JSON.parse(config_json);
       }
       return result;
@@ -354,7 +324,7 @@ class NetworkMonitorSensor extends Sensor {
     }
   }
 
-  async apiRun() {
+  async apiRun(){
     extensionManager.onGet("networkMonitorData", async (msg, data) => {
       return this.getNetworkMonitorData();
     });
@@ -362,28 +332,80 @@ class NetworkMonitorSensor extends Sensor {
 
   async recordSampleDataInRedis(monitorType, target, timeSlot, data) {
     const redisKey = `${KEY_PREFIX_RAW}:${monitorType}:${target}`;
-    log.info(`record sample data in ${redisKey} at ${timeSlot}`);
+    log.debug(`record sample data(${JSON.stringify(data,null,4)}) in ${redisKey} at ${timeSlot}`);
     try {
       const dataSorted = [...data].sort( (a,b) => {
         return a-b
       })
-      const dataLength = dataSorted.length;
-      const median = (dataLength%2 === 0) ? (dataSorted[dataLength/2-1]+dataSorted[dataLength/2])/2 : dataSorted[(dataLength-1)/2];
-      const result = {
-        "data": data,
-        "stat" : {
-          "median": median,
-          "min"   : dataSorted[0],
-          "max"   : dataSorted[dataLength-1]
+      const l = dataSorted.length;
+      if (l>0) {
+        const result = {
+          "data": data,
+          "stat" : {
+            "median": (l%2 === 0) ? (dataSorted[l/2-1]+dataSorted[l/2])/2 : dataSorted[(l-1)/2],
+            "min"   : dataSorted[0],
+            "max"   : dataSorted[l-1]
+          }
         }
+        const resultJSON = JSON.stringify(result);
+        await rclient.hsetAsync(redisKey, timeSlot, resultJSON);
       }
-      const resultJSON = JSON.stringify(result);
-      await rclient.hsetAsync(redisKey, timeSlot, resultJSON);
     } catch (err) {
       log.error("failed to record sample data of ${moitorType} for ${target} :", err);
     }
   }
 
+  async statCalulate(monitorType,target,cfg) {
+    log.info(`calculate stat of ${monitorType} for ${target} with cfg(${JSON.stringify(cfg,null,4)})`);
+    try {
+      // calculate between [timeSinceSlot , timeTillSlot]
+      const timeTill = Date.now()-cfg.sampleInterval*1000; // caculate with one sampleInterval delay
+      const timeTillSlot = (timeTill - timeTill % (cfg.sampleInterval*1000))/1000;
+      const timeSince = timeTill - 1000*cfg.expirePeriod;
+      const timeSinceSlot = (timeSince - timeSince%(cfg.sampleInterval*1000))/1000;
+
+      const rawKey = `${KEY_PREFIX_RAW}:${monitorType}:${target}`;
+      log.debug("timeSinceSlot=",timeSinceSlot);
+      log.debug("timeTillSlot=",timeTillSlot);
+      let allData = [];
+      for (let ts=timeTillSlot; ts>=timeSinceSlot; ts-=cfg.sampleInterval) {
+        const result_json = await rclient.hgetAsync(rawKey,ts);
+        if (result_json) {
+          log.debug(`rawKey=${rawKey}, ts=${ts}, result_json=${result_json}`);
+          const result = JSON.parse(result_json);
+          if (result && result.stat && result.stat.median) {
+            log.debug(`collect data into ${ts} at ${rawKey}`);
+            allData.push(result.stat.median);
+            log.debug("allData.length:",allData.length);
+          }
+        } else {
+          log.warn(`Data ${rawKey} misssing`);
+          break;
+        }
+      }
+      allData.sort((a,b) => a-b );
+      log.debug("sorted allData:",allData);
+      const l = allData.length;
+      if (l > 0) {
+        const statKey = `${KEY_PREFIX_STAT}:${monitorType}:${target}`;
+        log.info("record stat data at ",statKey);
+        await rclient.hsetAsync(statKey, "min", allData[0]);
+        await rclient.hsetAsync(statKey, "max", allData[l-1]);
+        await rclient.hsetAsync(statKey, "median", (l%2 === 0) ? (allData[l/2-1]+allData[l/2])/2 : allData[(l-1)/2]);
+      }
+
+    } catch (err) {
+      log.error("failed to calculate stats: ", err);
+    }
+  }
+
+  async scheduleStatJob(monitorType,ip,cfg) {
+    log.info(`scheduling stat job for ${monitorType} with ip(${ip}) and cfg(${JSON.stringify(cfg,null,4)})`);
+    const scheduledJob = setInterval(() => {
+      this.statCalulate(monitorType,ip,cfg);
+    }, 1000*cfg.statInterval);
+    return scheduledJob;
+  }
 }
 
 module.exports = NetworkMonitorSensor;
