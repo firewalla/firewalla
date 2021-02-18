90c032799198e13833e1450ffba8b64e7fee7d43
child <firewalla.zhijie@gmail.com>
2021-02-18 16:24:58 +0800

    put error in default case

--- controllers/netbot.js
+++ controllers/netbot.js
@@ -4108,13 +4108,12 @@ class netBot extends ControllerBot {
         break;
       case "beta":
         targetBranch = prodBranch.replace("release_", "beta_")
-        break
+        break;
       case "prod":
         targetBranch = prodBranch
-        break
-    }
-    if (!targetBranch) {
-      throw new Error("Can not switch to branch", targetBranch);
+        break;
+      default:
+        throw new Error("Can not switch to branch", target);
     }
     log.info("Going to switch to branch", targetBranch);
     try {
