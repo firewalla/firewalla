90c09ebe3136146821a7b365c2c6468f850f1df8
Jian Yang <pub@yangjian.net>
2021-02-23 20:28:30 -0800

    make parse_json=true as default for events listing APIs

--- event/EventApi.js
+++ event/EventApi.js
@@ -63,12 +63,12 @@ class EventApi {
         }
     }
 
-    async listLatestStateEventsAll(parse_json=false) {
+    async listLatestStateEventsAll(parse_json=true) {
         let result = null;
         try {
             result = await rclient.hgetallAsync(KEY_EVENT_STATE_CACHE);
-            if (parse_json) {
-                Object.keys(result).forEach( (k)=>{result[k] = JSON.parse(result[k]) });
+            if ( parse_json ) {
+              Object.keys(result).forEach( (k)=>{result[k] = JSON.parse(result[k]) });
             }
         } catch (err) {
             log.error("failed to get all saved state event requests:",err);
@@ -87,12 +87,12 @@ class EventApi {
         }
     }
 
-    async listLatestStateEventsError(parse_json=false) {
+    async listLatestStateEventsError(parse_json=true) {
         let result = null;
         try {
             result = await rclient.hgetallAsync(KEY_EVENT_STATE_CACHE_ERROR);
-            if (parse_json) {
-                Object.keys(result).forEach( (k)=>{result[k] = JSON.parse(result[k]) });
+            if ( parse_json ) {
+              Object.keys(result).forEach( (k)=>{result[k] = JSON.parse(result[k]) });
             }
         } catch (err) {
             log.error("failed to get all error state event requests:",err);
@@ -100,7 +100,7 @@ class EventApi {
         return result;
     }
 
-    async listEvents(min="-inf", max="inf", withscores=false, limit_offset=0, limit_count=-1, reverse=false) {
+    async listEvents(min="-inf", max="inf", withscores=false, limit_offset=0, limit_count=-1, reverse=false, parse_json=true) {
       let result = null
       try {
         log.info(`getting events from ${min} to ${max}`);
@@ -108,10 +108,10 @@ class EventApi {
         const params = withscores ?
           [KEY_EVENT_LOG, begin, end, "withscores","limit",limit_offset,limit_count] :
           [KEY_EVENT_LOG, begin, end, "limit",limit_offset,limit_count];
-        if (reverse) {
-          result = await rclient.zrevrangebyscoreAsync(params);
-        } else {
-          result = await rclient.zrangebyscoreAsync(params);
+        
+        result = reverse ? await rclient.zrevrangebyscoreAsync(params) : await rclient.zrangebyscoreAsync(params);
+        if ( result && parse_json ) {
+          result.forEach((x,idx)=>result[idx]=JSON.parse(x));
         }
       } catch (err) {
         log.error(`failed to get events between ${min} and ${max}, with limit offset(${limit_offset})/count(${limit_count}) and reverse(${reverse}), ${err}`);
