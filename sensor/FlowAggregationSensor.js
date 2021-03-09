435b2f59b3e7813b1fd8e14eab5504b7c8b1bf5f
Reinhard L <callmerein@gmail.com>
2021-03-09 19:08:13 +0800

    disable dns aggrflow
    (cherry picked from commit 53aaeb0116a3ebd0a8dbd2655d287f63dab9c7c9)

--- sensor/FlowAggregationSensor.js
+++ sensor/FlowAggregationSensor.js
@@ -658,9 +658,10 @@ class FlowAggregationSensor extends Sensor {
       await flowAggrTool.addFlows(macAddress, "dnsB", this.config.interval, end, groupedLogs.dns, this.config.aggrFlowExpireTime);
     }
     await flowAggrTool.addFlows(macAddress, "ipB", this.config.interval, end, groupedLogs.ip, this.config.aggrFlowExpireTime);
-    const dnsLogs = await auditTool.getDeviceLogs({ mac: macAddress, begin, end, block: false});
-    const groupedDnsLogs = this.auditLogsGroupByDestIP(dnsLogs);
-    await flowAggrTool.addFlows(macAddress, "dns", this.config.interval, end, groupedDnsLogs.dns, this.config.aggrFlowExpireTime);
+    // dns aggrflow, disable for now to reduce memory cost
+    // const dnsLogs = await auditTool.getDeviceLogs({ mac: macAddress, begin, end, block: false});
+    // const groupedDnsLogs = this.auditLogsGroupByDestIP(dnsLogs);
+    // await flowAggrTool.addFlows(macAddress, "dns", this.config.interval, end, groupedDnsLogs.dns, this.config.aggrFlowExpireTime);
   }
 
   async getFlow(dimension, type, options) {
