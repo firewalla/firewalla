e6054a90cd3689389d877f61118811811fed5f1d
Jian Yang <pub@yangjian.net>
2021-03-31 22:59:48 -0700

    add script to store tech support file in /data

---
+++ scripts/store_support.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# A script to store mini techsupport file 
+
+# ----------------------------------------------------------------------------
+# NOTES
+# ----------------------------------------------------------------------------
+# command to call techsupport: /home/pi/firewalla/scripts/techsupport short 10, which is the same as the techsupport when creating zendesk cases.
+# there will be a script to do the entire stuff, and it will be called by the reset script and bluetooth code.
+# * https://github.com/firewalla/firewalla/blob/99b29a3e524a57fd5f78d4596c3e05e46b0ac12a/scripts/system-reset-all-overlayfs-navy.sh
+# * https://github.com/firewalla/firewalla/blob/99b29a3e524a57fd5f78d4596c3e05e46b0ac12a/scripts/system-reset-all-overlayfs.sh
+# * bluetooth binary (firereset)
+# create a folder under /data to store the log tar.gz file. /data/support
+# the time to execute techsupport should be limited, need to figure out based on the real data.
+# /data/support should only keep last 3 versions of support files. (assume most of people won't reset for more the 3 times)
+# if /data partition free space is less than 200MB, skip storing the tech support file.
+# if remote support tar.gz file is over than 50MB, skip storing the tech support file.
+
+
+# ----------------------------------------------------------------------------
+# Constants
+# ----------------------------------------------------------------------------
+: ${FIREWALLA_HOME:='/home/pi/firewalla'}
+: ${FIREWALLA_TMP:='/home/pi/tmp'}
+STORE_DIR=/data/store_techsupport
+BACKUP_COUNT=3
+SUPPORT_FILE_NAME='support.tar.gz'
+RUN_TIMEOUT=300
+DISK_FREE_MIN=204800
+FILE_SIZE_MAX=512
+SUPPORT_FILE_PATH=$FIREWALLA_TMP/$SUPPORT_FILE_NAME
+SUPPORT_FILE_LIMIT=51200
+
+# ----------------------------------------------------------------------------
+# Functions
+# ----------------------------------------------------------------------------
+
+err() {
+    echo "ERROR: $@" >&2
+}
+
+run_techsupport() {
+    timeout -v $RUN_TIMEOUT $FIREWALLA_HOME/scripts/techsupport short 10
+}
+
+# ----------------------------------------------------------------------------
+# MAIN goes here
+# ----------------------------------------------------------------------------
+
+sudo mkdir -p $STORE_DIR
+sudo chown pi:pi $STORE_DIR
+
+cd $STORE_DIR
+
+# check disk space
+disk_free=$(df -Pk . | tail -1 | awk '{print $4}')
+test $disk_free -ge $DISK_FREE_LIMIT || {
+    err "free disk space in $PWD is less than $DISK_FREE_LIMIT, skip storing"
+    exit 1
+}
+
+run_techsupport || {
+    err failed to run techsupport
+    exit 1
+}
+
+# check file size
+support_file_size=$(stat -c %s $SUPPORT_FILE_PATH)
+test $support_file_size -le $SUPPORT_FILE_LIMIT || {
+    err "$SUPPORT_FILE_PATH size($support_file_size) is over limit($SUPPORT_FILE_LIMIT), skip storing"
+    exit 1
+}
+
+# rotate existing files if any
+for i in $(seq $BACKUP_COUNT -1 1)
+do
+    let j=i-1
+    test -e ${SUPPORT_FILE_NAME}.$j && mv -f ${SUPPORT_FILE_NAME}.{$j,$i} 
+done
+cp -f $SUPPORT_FILE_PATH ${SUPPORT_FILE_NAME}.1
+
+ls -l
+echo "Tech support file stored successfully"
+
+exit 0
