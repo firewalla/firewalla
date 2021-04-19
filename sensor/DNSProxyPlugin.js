41f7c8e0f9ea59a523de36d7c8bf9caee7ee868a
Melvin Tu <melvinto@gmail.com>
2021-04-19 18:55:01 +0800

    bugfix

--- sensor/DNSProxyPlugin.js
+++ sensor/DNSProxyPlugin.js
@@ -75,8 +75,10 @@ class DNSProxyPlugin extends Sensor {
     this.server = dgram.createSocket('udp4');
 
     this.server.on('message', async (msg, info) => {
-      let req = this.parseRequest(msg);
-      await this.processRequest(req);
+      const qname = this.parseRequest(msg);
+      if(qname) {
+        await this.processRequest(qname);        
+      }
       // never need to reply back to client as this is not a true dns server
     });
 
@@ -94,113 +96,22 @@ class DNSProxyPlugin extends Sensor {
   parseRequest(req) {
     //see rfc1035 for more details
     //http://tools.ietf.org/html/rfc1035#section-4.1.1
-    
-    var query = {};
-    query.header = {};
-    //TODO write code to break questions up into an array
-    query.question = {};
-
-    var tmpSlice;
-    var tmpByte;
-    
-    //transaction id
-    // 2 bytes
-    query.header.id = req.slice(0,2);
-
-    //slice out a byte for the next section to dice into binary.
-    tmpSlice = req.slice(2,3);
-    //convert the binary buf into a string and then pull the char code
-    //for the byte
-    tmpByte = tmpSlice.toString('binary', 0, 1).charCodeAt(0);
-    
-    //qr
-    // 1 bit
-    query.header.qr = sliceBits(tmpByte, 0,1);
-    //opcode
-    // 0 = standard, 1 = inverse, 2 = server status, 3-15 reserved
-    // 4 bits
-    query.header.opcode = sliceBits(tmpByte, 1,4);
-    //authorative answer
-    // 1 bit
-    query.header.aa = sliceBits(tmpByte, 5,1);
-    //truncated
-    // 1 bit
-    query.header.tc = sliceBits(tmpByte, 6,1);
-    //recursion desired
-    // 1 bit
-    query.header.rd = sliceBits(tmpByte, 7,1);
-
-    //slice out a byte to dice into binary
-    tmpSlice = req.slice(3,4);
-    //convert the binary buf into a string and then pull the char code
-    //for the byte
-    tmpByte = tmpSlice.toString('binary', 0, 1).charCodeAt(0);
-    
-    //recursion available
-    // 1 bit
-    query.header.ra = sliceBits(tmpByte, 0,1);
-
-    //reserved 3 bits
-    // rfc says always 0
-    query.header.z = sliceBits(tmpByte, 1,3);
-
-    //response code
-    // 0 = no error, 1 = format error, 2 = server failure
-    // 3 = name error, 4 = not implemented, 5 = refused
-    // 6-15 reserved
-    // 4 bits
-    query.header.rcode = sliceBits(tmpByte, 4,4);
-
-    //question count
-    // 2 bytes
-    query.header.qdcount = req.slice(4,6);
-    //answer count
-    // 2 bytes
-    query.header.ancount = req.slice(6,8);
-    //ns count
-    // 2 bytes
-    query.header.nscount = req.slice(8,10);
-    //addition resources count
-    // 2 bytes
-    query.header.arcount = req.slice(10, 12);
-    
-    //assuming one question
-    //qname is the sequence of domain labels
-    //qname length is not fixed however it is 4
-    //octets from the end of the buffer
-    query.question.qname = req.slice(12, req.length - 4);
-    //qtype
-    query.question.qtype = req.slice(req.length - 4, req.length - 2);
-    //qclass
-    query.question.qclass = req.slice(req.length - 2, req.length);
-    
-    return query;
+    if(req.length < 16) {
+      return null;
+    }
+    return req.slice(12, req.length - 4);
   }
 
   getKey(domain) {
     return `intel:dns:${domain}`;
   }
   
-  async checkCache(domain) {
-    const domains = flowUtil.getSubDomains(domain);
-    if(!domains) {
-      log.warn("Invalid Domain", domain, "skipped.");
-      return;
-    }
-
-    for(const dn of domains) {
-      const key = this.getKey(dn);
-      const info = await rclient.hgetallAsync(key);
-      if(!_.isEmpty(info)) {
-        return info;
-      }
-    }
-
-    return null;
+  async checkCache(domain) { // only return if there is exact-match intel in redis
+    const key = this.getKey(domain);
+    return await rclient.hgetallAsync(key);
   }
 
-  async processRequest(req) {
-    const qname = req.question.qname;
+  async processRequest(qname) {
     const domain = qnameToDomain(qname);
     log.info("dns request is", domain);
     const begin = new Date() / 1;
@@ -223,11 +134,19 @@ class DNSProxyPlugin extends Sensor {
         return;
       }
 
-      const lastDN = domains[domains.length - 1];
-      const key = this.getKey(lastDN);
-      await rclient.hmsetAsync(key, {c: 'x', a: '1'});
-      await rclient.saddAsync(allowKey, lastDN);
-      await rclient.expire(key, expireTime);
+      // since result is empty, it means all sub domains of this domain are good
+      for(const dn of domains) {
+        const key = this.getKey(dn);
+        await rclient.hmsetAsync(key, {c: 'x', a: '1'});
+        await rclient.expire(key, expireTime);
+      }
+
+      // only last dn be added to allow key for better performance
+      if(domains.length > 0) {
+        const dn = domains[domains.length - 1];
+        await rclient.saddAsync(allowKey, dn);
+      }
+
     } else {
       for(const item of result) {
         const dn = item.originIP; // originIP is the domain
