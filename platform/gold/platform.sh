9fa9d2f112351eddb628e81c5618243dd164b586
Jian Yang <pub@yangjian.net>
2021-07-20 04:02:51 -0700

    move run_horse_light out of firewalla platform

--- platform/gold/platform.sh
+++ platform/gold/platform.sh
@@ -77,18 +77,6 @@ function map_target_branch {
   esac
 }
 
-function run_horse_light {
-  flash_interval=${1:-2}
-  pause_interval=${2:-1}
-  sudo pkill -9 ethtool
-  for ((i=3;i>=0;i--))
-  do
-    sudo pkill -9 ethtool
-    sudo timeout $flash_interval ethtool -p eth${i}
-    sleep $pause_interval
-  done
-}
-
 function fw_blink {
   sudo pkill -9 ethtool
   sudo timeout 3600s ethtool -p $1 &
