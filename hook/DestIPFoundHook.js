41e9e8e5439995208139d56e13a2d4c4d441231b
Melvin Tu <melvinto@gmail.com>
2021-08-05 19:16:16 +0800

    should return true to ensure it will query cloud for intel

--- hook/DestIPFoundHook.js
+++ hook/DestIPFoundHook.js
@@ -305,7 +305,7 @@ class DestIPFoundHook extends Hook {
 
       const rpResult = await rp(options).catch((err) => {
         log.error("got error when calling intel proxy, err:", err.message, "d:", query);
-        return {result: false};
+        return {result: true};
       });
       
       const matched = rpResult && rpResult.result; // { "result": true }
