0b818f0caf04c5065a9bab773f177b58b9187d6b
Melvin Tu <melvinto@gmail.com>
2021-10-21 23:11:18 +0800

    fixed bug on handling web member update

--- mgmt/FireWeb.js
+++ mgmt/FireWeb.js
@@ -119,10 +119,10 @@ class FireWeb {
         // remove from revoked eid set
         await rclient.sremAsync(Constants.REDIS_KEY_EID_REVOKE_SET, eptCloud.eid);
 
-        const eptCloudExtension = new EptCloudExtension(eptCloud, gid);
-        await eptCloudExtension.recordAllRegisteredClients(gid).catch((err) => {
-          log.error("Failed to record registered clients, err:", err);
-        });
+        (async () => {
+          const eptCloudExtension = new EptCloudExtension(eptCloud, gid);
+          await eptCloudExtension.updateGroupInfo(gid);
+        })();
 
         return;
       } catch(err) {
