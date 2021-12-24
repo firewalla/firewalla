d288e942ca467d745bf29d8125bf4860c769a28c
Jian Yang <pub@yangjian.net>
2021-12-24 01:20:10 -0800

    patch in pf_ring.ko for image based on 5.4.0-88-generic kernel

---
+++ platform/gold/hooks/before_main_run/07_add_pf_ring_for_kernl_5_4_0_88.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+KERNEL_VERSION='5.4.0-88-generic'
+KO_DIR="/lib/modules/$KERNEL_VERSION/kernel/net/pf_ring"
+KO_PATH="$KO_DIR/pf_ring.ko"
+test $(uname -r) == $KERNEL_VERSION || exit 1
+sudo mkdir -p $KO_DIR
+sudo cp -f ${CUR_DIR}/../../files/pf_ring-${KERNEL_VERSION}.ko $KO_PATH
+sudo depmod -a
+lsmod | grep -q pf_ring && sudo modprobe -r pf_ring
+sudo modprobe pf_ring
+
+exit 0
