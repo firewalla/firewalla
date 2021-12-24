dd38dc82ef9e5891391ad6380f52b339eb81aa9b
Jian Yang <pub@yangjian.net>
2021-12-24 01:27:14 -0800

    set CUR_DIR

--- platform/gold/hooks/before_main_run/07_add_pf_ring_for_kernl_5_4_0_88.sh
+++ platform/gold/hooks/before_main_run/07_add_pf_ring_for_kernl_5_4_0_88.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 KERNEL_VERSION='5.4.0-88-generic'
 KO_DIR="/lib/modules/$KERNEL_VERSION/kernel/net/pf_ring"
 KO_PATH="$KO_DIR/pf_ring.ko"
+
 test $(uname -r) == $KERNEL_VERSION || exit 1
 sudo mkdir -p $KO_DIR
 sudo cp -f ${CUR_DIR}/../../files/pf_ring-${KERNEL_VERSION}.ko $KO_PATH
