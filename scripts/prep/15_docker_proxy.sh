de9486cfac44587e8b833c5abc12876521c0b7e8
Melvin Tu <melvinto@gmail.com>
2022-01-25 15:36:43 +0800

    typo

--- scripts/prep/15_docker_proxy.sh
+++ scripts/prep/15_docker_proxy.sh
@@ -2,6 +2,6 @@
 
 test -e /home/pi/.docker.http-proxy.conf || exit 0
 
-sudo cp -f /home/pi/.docker.http-proxy.conf /etc/sysetmd/system/docker.service.d/http-proxy.conf
+sudo cp -f /home/pi/.docker.http-proxy.conf /etc/systemd/system/docker.service.d/http-proxy.conf
 sudo systemctl daemon-reload
 sudo systemctl is-active docker &>/dev/null && sudo systemctl restart docker
