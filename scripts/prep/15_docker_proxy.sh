954bc2f5bd516ad5d2be2e23ea78a51832cdd6c2
Melvin Tu <melvinto@gmail.com>
2022-01-25 15:41:59 +0800

    update

--- scripts/prep/15_docker_proxy.sh
+++ scripts/prep/15_docker_proxy.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# run as root/sudo
+
 SRC=/home/pi/.docker.http-proxy.conf
 DST=/etc/systemd/system/docker.service.d/http-proxy.conf
 
 test -e $SRC || exit 0
 
 mkdir -p $(dirname $DST)
-sudo cp -f $SRC $DST
-sudo systemctl daemon-reload
-sudo systemctl is-active docker &>/dev/null && sudo systemctl restart docker
+cp -f $SRC $DST
+systemctl daemon-reload
+systemctl is-active docker &>/dev/null && sudo systemctl restart docker
