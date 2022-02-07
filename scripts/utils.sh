6a27bbdd5cc9a92bbdf0c8193588426d424bd31e
Edison Liu <jasonlyc.sjtu@gmail.com>
2022-02-07 11:35:30 +0800

    recursively set owner/group to pi on runtime folder

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -21,11 +21,8 @@ function setup_folders() {
     mkdir -p ~/.firewalla/run/docker_vpn_client/clash
     mkdir -p ~/.firewalla/run/zeek/scripts
     mkdir -p ~/.firewalla/run/assets
-    # in case leftover docker containers are automatically started after reset, need to restore the owner/group on the following directory
-    sudo chown pi /home/pi/.firewalla/run
-    sudo chgrp pi /home/pi/.firewalla/run
-    sudo chown pi /home/pi/.firewalla/run/docker_vpn_client
-    sudo chgrp pi /home/pi/.firewalla/run/docker_vpn_client
+    # in case leftover docker containers are automatically started after reset, need to restore the owner/group on the runtime directory
+    sudo chown -R pi:pi /home/pi/.firewalla/run
     test -e ~/.firewalla/.sshpasswd && sudo chown pi ~/.firewalla/.sshpasswd
     : ${FIREWALLA_HOME:=/home/pi/firewalla}
 
