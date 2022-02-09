24fa69cebd1f52213f55f216100e1067a08d0b9b
Melvin Tu <melvinto@gmail.com>
2022-02-10 00:09:47 +0800

    fix copyright
    (cherry picked from commit c87d9852bdee47a67fa59518f5c8b4019c63cbf8)

--- alarm/TrustManager.js
+++ alarm/TrustManager.js
@@ -1,4 +1,4 @@
-/*    Copyright 2016-2022 Firewalla Inc.
+/*    Copyright 2022 Firewalla Inc.
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
