8243e487b1563ae64630b8eb384f039a13e71da9
Reinhard L <callmerein@gmail.com>
2022-03-18 18:53:30 +0800

    add SysPatchSensor

---
+++ scripts/patch_system.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+: ${FIREWALLA_HOME:=/home/pi/firewalla}
+: ${FIREWALLA_HIDDEN:=/home/pi/.firewalla}
+
+source ${FIREWALLA_HOME}/platform/platform.sh
+
+cd /data/patch/deb/
+[[ $? == 0 ]] && for FILE in $(ls); do
+  # file name should be the package name without extension
+  VERSION=$(dpkg -I $FILE | grep Version: | cut -d':' -f2-)
+  INSTALLED=$(apt-cache policy $FILE | grep Installed: | cut -d':' -f2-)
+  if [[ "$VERSION" == "$INSTALLED" ]]; then
+    echo "$FILE has $INSTALLED installed, skip"
+    continue
+  fi
+
+  sudo dpkg -i $FILE
+done
