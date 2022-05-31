10cc8d8555dc3685c4e751f57e1c495326648a8d
melvin <108800+MelvinTo@users.noreply.github.com>
2022-05-31 16:24:21 +0800

    bugfix

--- sensor/GuardianSensor.js
+++ sensor/GuardianSensor.js
@@ -297,6 +297,7 @@ class GuardianSensor extends Sensor {
 
   async enable_key_rotation() {
     await delay(5 * 1000);
+    const gid = await et.getGID();
     await fc.enableDynamicFeature("rekey");
     await cw.getCloud().reKeyForAll(gid);
   }
