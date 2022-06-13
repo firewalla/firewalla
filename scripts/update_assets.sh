7094e86b7ed704a39a0f51e49167140100096c4d
Ye Xia <only.one.truth.1102@gmail.com>
2022-06-13 13:38:43 +0800

    Add asset signature check

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -15,7 +15,16 @@ if [[ ! -d $ASSETSD_PATH ]]; then
 fi
 
 ASSETS_PREFIX=$(get_assets_prefix)
+RELEASE_TYPE=$(get_release_type)
 
+### can set VERIFY_SIGNATURE to "false" in dev to bypass signature check
+if [ -z "$VERIFY_SIGNATURE" -o "$RELEASE_TYPE" = "alpha" -o "$RELEASE_TYPE" = "beta" -o "$RELEASE_TYPE" = "prod" ]; then
+  VERIFY_SIGNATURE=true
+fi
+
+ASSETS_PUBLIC_KEY=/home/pi/firewalla/etc/keys/assets.key
+TEMP_DIR=$(mktemp -d)
+trap 'rm -fr "$TEMP_DIR"' EXIT
 
 cd $ASSETSD_PATH
 # unify lists under assets.d/, keeps one entry for each file only
@@ -25,8 +34,10 @@ while IFS= read -r line; do
   line=$(eval 'for param in '$line'; do echo $param; done')
   IFS=$'\n' read -rd '' -a params <<< "$line"
   file_path=${params[0]}
-  bin_url="${ASSETS_PREFIX}${params[1]}"
+  s3_path=${params[1]}
+  bin_url="${ASSETS_PREFIX}${s3_path}"
   hash_url="${bin_url}.sha256"
+  signature_url="${bin_url}.sig"
   perm=${params[2]}
   exec_pre=${params[3]}
   exec_post=${params[4]}
@@ -39,6 +50,23 @@ while IFS= read -r line; do
     echo "Invalid hash from $hash_url"
     continue
   fi
+
+  # verify signature
+  if [ "$VERIFY_SIGNATURE" = "true" ]; then
+    signature_file="$TEMP_DIR"/$(cat /dev/urandom | tr -dc '[:alpha:]' |  head -c 20)
+    wget -qO "$signature_file" "$signature_url"
+    if [ "$?" != 0 ]; then
+      echo "No signature file found: $signature_url"
+      continue
+    fi
+    SIGNED_TEXT="$expected_hash,${s3_path}"
+    echo -n "$SIGNED_TEXT" | openssl dgst -verify "$ASSETS_PUBLIC_KEY" -keyform PEM -sha256 -signature "$signature_file" > /dev/null
+    if [ "$?" != 0 ]; then
+      echo "Error signature: ${s3_path}"
+      continue
+    fi
+  fi
+
   current_hash=""
   if [[ -f $file_path ]]; then
     current_hash=$(sha256sum $file_path | awk '{print $1}')
@@ -46,6 +74,16 @@ while IFS= read -r line; do
   changed=""
   if [[ $expected_hash != $current_hash ]]; then
     echo "Hash of $file_path mismatches with $hash_url, will fetch latest file from $bin_url"
+
+    sudo mkdir -p $(dirname "${file_path}")
+    temp_file="$file_path".download
+    sudo wget "$bin_url" -O "$temp_file"
+    verify_hash=$(sha256sum $temp_file | awk '{print $1}')
+    if [[ "$verify_hash" != "$expected_hash" ]]; then
+      echo "Incomplete file downloaded"
+      continue
+    fi
+
     if [[ -n $exec_pre ]]; then
       eval "$exec_pre"
       # if [ $? -ne 0 ]; then
@@ -53,8 +91,8 @@ while IFS= read -r line; do
       #   continue; # skip if command fails
       # fi
     fi
-    sudo mkdir -p $(dirname "${file_path}")
-    sudo wget $bin_url -O $file_path
+
+    sudo mv "$temp_file" "$file_path"
     changed="1"
   else
     echo "Hash of $file_path matches with $hash_url"
