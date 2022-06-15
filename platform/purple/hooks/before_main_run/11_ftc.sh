99a8dd5b189f53e2360fa50d00b17584779657e0
melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-15 17:30:25 +0800

    update

--- platform/purple/hooks/before_main_run/11_ftc.sh
+++ platform/purple/hooks/before_main_run/11_ftc.sh
@@ -17,5 +17,5 @@ if ! cmp -s ${CUR_DIR}/../../files/fire-temp-check.sh /usr/local/bin/fire-temp-c
     sudo cp ${CUR_DIR}/../../files/ftc.* /etc/systemd/system/
     sudo ln -sf /etc/systemd/system/ftc.timer /etc/systemd/system/timers.target.wants/ftc.timer
     sudo systemctl daemon-reload
-    sudo systemctl start ftc
+    sudo systemctl start ftc.timer
 fi
