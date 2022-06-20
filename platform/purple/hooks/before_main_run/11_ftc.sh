23b1fb5ccc429dbc589ba3c24aedf208c4e3c971
melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-20 12:54:17 +0800

    update ftc

--- platform/purple/hooks/before_main_run/11_ftc.sh
+++ platform/purple/hooks/before_main_run/11_ftc.sh
@@ -9,13 +9,13 @@ ROOT_RO=/media/root-ro
 if ! cmp -s ${CUR_DIR}/../../files/fire-temp-check.sh ${ROOT_RO}/usr/local/bin/fire-temp-check.sh; then
     sudo install ${CUR_DIR}/../../files/fire-temp-check.sh ${ROOT_RO}/usr/local/bin/
     sudo cp ${CUR_DIR}/../../files/ftc.* ${ROOT_RO}/etc/systemd/system/
-    sudo ln -sf /etc/systemd/system/ftc.timer ${ROOT_RO}/etc/systemd/system/timers.target.wants/ftc.timer
+    sudo ln -sf /etc/systemd/system/ftc.service ${ROOT_RO}/etc/systemd/system/multi-user.target.wants/ftc.service
 fi
 
 if ! cmp -s ${CUR_DIR}/../../files/fire-temp-check.sh /usr/local/bin/fire-temp-check.sh; then
     sudo install ${CUR_DIR}/../../files/fire-temp-check.sh /usr/local/bin/
     sudo cp ${CUR_DIR}/../../files/ftc.* /etc/systemd/system/
-    sudo ln -sf /etc/systemd/system/ftc.timer /etc/systemd/system/timers.target.wants/ftc.timer
+    sudo ln -sf /etc/systemd/system/ftc.service /etc/systemd/system/multi-user.target.wants/ftc.service
     sudo systemctl daemon-reload
     sudo systemctl start ftc.timer
 fi
