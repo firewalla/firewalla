627b770a5f8f7e05662f28df501a06bab85c6bed
melvin <108800+MelvinTo@users.noreply.github.com>
2022-06-20 17:59:47 +0800

    bugfix

--- platform/purple/hooks/before_main_run/11_ftc.sh
+++ platform/purple/hooks/before_main_run/11_ftc.sh
@@ -6,13 +6,20 @@ test -e ${CUR_DIR}/../../files/fire-temp-check.sh || exit 0
 
 ROOT_RO=/media/root-ro
 
+CHANGED=0
+
 if ! cmp -s ${CUR_DIR}/../../files/fire-temp-check.sh ${ROOT_RO}/usr/local/bin/fire-temp-check.sh; then
     sudo install ${CUR_DIR}/../../files/fire-temp-check.sh ${ROOT_RO}/usr/local/bin/
     sudo cp ${CUR_DIR}/../../files/ftc.* ${ROOT_RO}/etc/systemd/system/
     sudo ln -sf /etc/systemd/system/ftc.service ${ROOT_RO}/etc/systemd/system/multi-user.target.wants/ftc.service
+    CHANGED=1
 fi
 
 if ! cmp -s ${CUR_DIR}/../../files/fire-temp-check.sh /usr/local/bin/fire-temp-check.sh; then
+    CHANGED=1
+fi
+
+if [[ "$CHANGED" -eq 1 ]]; then
     sudo install ${CUR_DIR}/../../files/fire-temp-check.sh /usr/local/bin/
     sudo cp ${CUR_DIR}/../../files/ftc.* /etc/systemd/system/
     sudo ln -sf /etc/systemd/system/ftc.service /etc/systemd/system/multi-user.target.wants/ftc.service
