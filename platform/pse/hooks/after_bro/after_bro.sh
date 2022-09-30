82692232cdfd603762531fc23d472d47d2d8311b
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2022-09-30 18:43:07 +0800

    pse platform adaption

--- platform/pse/hooks/after_bro/after_bro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /bspool/
-ln -sfT bro zeek
--- platform/pse/hooks/before_bro/before_bro.sh
+++ platform/pse/hooks/before_bro/before_bro.sh
@@ -2,17 +2,17 @@
 
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-if [[ ! -e /log/blog || ! -L /log/blog ]]; then
-  sudo rm -rf /log/blog
-  sudo ln -sfT /blog /log/blog
+if [[ -e /log/blog ]]; then
+  sudo ln -sfT /log/blog/current /blog/current
 fi
 
-[[ -e $CUR_DIR/broctl.cfg ]] && sudo cp $CUR_DIR/broctl.cfg /usr/local/zeek/etc/zeekctl.cfg
+[[ -e $CUR_DIR/broctl.cfg ]] && sudo cp $CUR_DIR/broctl.cfg /usr/local/bro/etc/zeekctl.cfg
 
 # check conflict on bro listen port and change default port if necessary
-for p in $(seq 47760 1 65520); do
-  if ! sudo netstat -anlp | grep 127.0.0.1 | grep "ESTABLISHED\|LISTEN" | awk '{print $4" "$7}' | grep -v zeek | grep "127.0.0.1:$p"; then
-    sudo bash -c "echo 'ZeekPort = $p' >> /usr/local/zeek/etc/zeekctl.cfg"
+NO_OF_INSTANCES=$(cat /usr/local/zeek/etc/node.cfg | grep "^type=" | wc -l)
+for p in $(seq 4776 1 6552); do
+  if ! sudo netstat -anlp | grep 127.0.0.1 | grep "ESTABLISHED\|LISTEN" | awk '{print $4" "$7}' | grep -v zeek | grep "127.0.0.1:$p[1-$NO_OF_INSTANCES]"; then
+    sudo bash -c "echo 'ZeekPort = ${p}0' >> /usr/local/zeek/etc/zeekctl.cfg"
     break
   fi
 done
@@ -22,7 +22,7 @@ OVERLAY_IP=$(ip addr show dev eth0 | awk '/inet /'  | grep -vw secondary | awk '
 
 if [[ -n "$EXTERNAL_IP" ]]; then
 
-  sudo bash -c 'cat >> /usr/local/zeek/share/zeek/site/local.zeek' <<EOS
+  sudo bash -c 'cat >> /usr/local/bro/share/bro/site/local.bro' <<EOS
 
 # local filter
 redef restrict_filters += [["not-itself"] = "not (host $EXTERNAL_IP and not port 53 and not port 8853)"];
@@ -31,7 +31,7 @@ fi
 
 if [[ -n "$OVERLAY_IP" ]]; then
 
-  sudo bash -c 'cat >> /usr/local/zeek/share/zeek/site/local.zeek' <<EOS
+  sudo bash -c 'cat >> /usr/local/bro/share/bro/site/local.bro' <<EOS
 
 # overlay filter
 redef restrict_filters += [["not-itself-overlay"] = "not (host $OVERLAY_IP and tcp)"];
