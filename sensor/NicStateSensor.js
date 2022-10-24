4f137d24e48e5b0d67b23990fded61261eee8c2d
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2022-10-24 11:07:59 +0800

    use different state values for different abnormal link speeds and duplex

--- sensor/NicStateSensor.js
+++ sensor/NicStateSensor.js
@@ -22,6 +22,21 @@ const platform = platformLoader.getPlatform();
 const era = require('../event/EventRequestApi.js');
 const Constants = require('../net2/Constants.js');
 
+const stateVals = {
+  // bit 1 - 3 reserved for speed
+  "speed": {
+    "10": 0x2,
+    "100": 0x4,
+    "1000": 0x6,
+    "2500": 0x8
+  },
+  // bit 0 - 0 reserved for duplex
+  "duplex": {
+    "full": 0x0,
+    "half": 0x1
+  }
+};
+
 class NicStateSensor extends Sensor {
   async run() {
     setInterval(() => {
@@ -37,15 +52,26 @@ class NicStateSensor extends Sensor {
       const state = states[nic];
       const maxSpeed = await platform.getMaxLinkSpeed(nic);
       state.maxSpeed = maxSpeed;
-      if (maxSpeed && state.carrier == "1" && state.speed) {
+      if (maxSpeed && state.carrier == "1" && !isNaN(state.speed) && Number(state.speed) > 0) {
         if (state.speed < maxSpeed || state.duplex && state.duplex != "full") {
-          era.addStateEvent(Constants.STATE_EVENT_NIC_STATE, nic, 1, {iface: nic, carrier: state.carrier, speed: state.speed, maxSpeed: maxSpeed, duplex: state.duplex}).catch((err) => {});
+          // each abnormal value in state object will have different bits set in state value
+          era.addStateEvent(Constants.STATE_EVENT_NIC_STATE, nic, this.getStateVal(state), {iface: nic, carrier: state.carrier, speed: state.speed, maxSpeed: maxSpeed, duplex: state.duplex}).catch((err) => {});
           continue;
         }
       }
       // if carrier is 0, or speed/maxSpeed is unavailable, or speed matches with the maxSpeed, set the state to 0
       era.addStateEvent(Constants.STATE_EVENT_NIC_STATE, nic, 0, {iface: nic, carrier: state.carrier, speed: state.speed, maxSpeed: maxSpeed, duplex: state.duplex}).catch((err) => {});
     }
+  }
+
+  getStateVal(state) {
+    let val = 0;
+    for (const key of Object.keys(state)) {
+      if (stateVals.hasOwnProperty(key)) {
+        val += stateVals[key][state[key]] || 0;
+      }
+    }
+    return val;
   }
 }
 
