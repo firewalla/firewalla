4ae07363461ecd1b404015e2519e38e0433ebe44
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2022-10-25 16:37:56 +0800

    add prev_state_value into event in EventRequestHandler.js

--- sensor/NicStateSensor.js
+++ sensor/NicStateSensor.js
@@ -22,21 +22,6 @@ const platform = platformLoader.getPlatform();
 const era = require('../event/EventRequestApi.js');
 const Constants = require('../net2/Constants.js');
 
-const stateVals = {
-  // bit 1 - 3 reserved for speed
-  "speed": {
-    "10": 0x2,
-    "100": 0x4,
-    "1000": 0x6,
-    "2500": 0x8
-  },
-  // bit 0 - 0 reserved for duplex
-  "duplex": {
-    "full": 0x0,
-    "half": 0x1
-  }
-};
-
 class NicStateSensor extends Sensor {
   async run() {
     setInterval(() => {
@@ -54,19 +39,12 @@ class NicStateSensor extends Sensor {
       state.maxSpeed = maxSpeed;
       const eventObj = {iface: nic, carrier: state.carrier, speed: state.speed, maxSpeed: maxSpeed, duplex: state.duplex};
       // do not emit state event if carrier is 0 or speed is unavailable
-      if (maxSpeed && state.carrier == "1" && !isNaN(state.speed) && Number(state.speed) > 0) {
+      if (maxSpeed && !isNaN(maxSpeed) && state.carrier == "1" && !isNaN(state.speed) && Number(state.speed) > 0) {
         // each abnormal value in state object will have different bits set in state value
-        era.addStateEvent(Constants.STATE_EVENT_NIC_SPEED, nic, this.getStateVal(state, "speed", maxSpeed), eventObj).catch((err) => {});
+        eventObj.ok_value = Number(maxSpeed);
+        era.addStateEvent(Constants.STATE_EVENT_NIC_SPEED, nic, Number(state.speed), eventObj).catch((err) => {});
       }
     }
-  }
-
-  getStateVal(state, type, expect) {
-    let val = 0;
-    if (state.hasOwnProperty(type) && stateVals.hasOwnProperty(type) && state[type] != expect) {
-      val = stateVals[type][state[type]] || 0;
-    }
-    return val;
   }
 }
 
