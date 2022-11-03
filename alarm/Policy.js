1d2c3452446b0c77316c31cd2fe2e4bd87a128cc
child <firewalla.zhijie@gmail.com>
2022-11-03 10:44:22 +0800

    add msp_id to policy compare fields

--- alarm/Policy.js
+++ alarm/Policy.js
@@ -170,7 +170,7 @@ class Policy {
     const compareFields = ["type", "target", "expire", "cronTime", "remotePort",
       "localPort", "protocol", "direction", "action", "upnp", "dnsmasq_only", "trust", "trafficDirection",
       "transferredBytes", "transferredPackets", "avgPacketBytes", "parentRgId", "targetRgId",
-      "ipttl", "wanUUID", "owanUUID", "seq", "routeType", "resolver", "origDst", "origDport", "snatIP", "flowIsolation"];
+      "ipttl", "wanUUID", "owanUUID", "seq", "routeType", "resolver", "origDst", "origDport", "snatIP", "flowIsolation", "msp_id"];
 
     for (const field of compareFields) {
       if (!this.isEqual(this[field], policy[field])) {
