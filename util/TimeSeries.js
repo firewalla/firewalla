bf0b67ff9bd3d4c276dccdf66e5946eae2ec8a40
child <firewalla.zhijie@gmail.com>
2022-11-28 15:44:34 +0800

    revert format changes

--- util/TimeSeries.js
+++ util/TimeSeries.js
@@ -14,6 +14,7 @@
  */
 'use strict';
 const TimeSeries = require('redis-timeseries')
+
 const log = require('../net2/logger.js')(__filename, 'info');
 const rclient = require('../util/redis_manager.js').getMetricsRedisClient();
 const sclient = require('../util/redis_manager.js').getSubscriptionClient();
@@ -32,7 +33,7 @@ sclient.subscribe(Message.MSG_SYS_TIMEZONE_RELOADED);
 const moment = require('moment-timezone');
 
 // Get current timestamp in seconds
-var getCurrentTime = function () {
+var getCurrentTime = function() {
   return Math.floor(Date.now() / 1000);
 };
 
@@ -40,7 +41,7 @@ var getCurrentTime = function () {
 var getRoundedTime = function (precision, time, flag) {
   time = time || getCurrentTime();
   let ts = Math.floor(time / precision) * precision;
-  if (!flag) return ts; // if it is keyTimestamp, return ts directly
+  if (flag) return ts; // if it is keyTimestamp, return ts directly
   let timeDate, tsDate;
   if (!timezone) {
     timeDate = moment(time * 1000).get('date');
@@ -76,69 +77,67 @@ var getRoundedTime = function (precision, time, flag) {
  * `timestamp` should be in seconds, and defaults to current time.
  * `increment` should be an integer, and defaults to 1
  */
-TimeSeries.prototype.recordHit = function (key, timestamp, increment, callback) {
+ TimeSeries.prototype.recordHit = function(key, timestamp, increment, callback) {
   var self = this;
 
-  Object.keys(this.granularities).forEach(function (gran) {
+  Object.keys(this.granularities).forEach(function(gran) {
     var properties = self.granularities[gran],
-      keyTimestamp = getRoundedTime(properties.precision || properties.ttl, timestamp), // high prority: precision
-      tmpKey = [self.keyBase, key, gran, keyTimestamp].join(':'),
-      hitTimestamp = getRoundedTime(properties.duration, timestamp, true);
+        keyTimestamp = getRoundedTime(properties.precision || properties.ttl, timestamp,true), // high prority: precision
+        tmpKey = [self.keyBase, key, gran, keyTimestamp].join(':'),
+      hitTimestamp = getRoundedTime(properties.duration, timestamp);
 
-    if (self.noMulti) {
-      self.redis.hincrby(tmpKey, hitTimestamp, Math.floor(increment || 1), (err) => {
-        if (err) {
-          if (callback) {
-            callback(err)
-          }
-          return
+   if(self.noMulti) {
+    self.redis.hincrby(tmpKey, hitTimestamp, Math.floor(increment || 1), (err) => {
+      if(err) {
+        if(callback) {
+          callback(err)
         }
-        self.redis.expireat(tmpKey, keyTimestamp + 2 * properties.ttl, (err2) => {
-          if (callback) {
-            callback(err2)
-          }
-        });
+        return
+      }
+      self.redis.expireat(tmpKey, keyTimestamp + 2 * properties.ttl, (err2) => {
+        if(callback) {
+          callback(err2)
+        }
       });
-    } else {
-      self.pendingMulti.hincrby(tmpKey, hitTimestamp, Math.floor(increment || 1));
-      self.pendingMulti.expireat(tmpKey, keyTimestamp + 2 * properties.ttl);
-    }
+    });
+   } else {
+    self.pendingMulti.hincrby(tmpKey, hitTimestamp, Math.floor(increment || 1));
+    self.pendingMulti.expireat(tmpKey, keyTimestamp + 2 * properties.ttl);
+   }
   });
 
   return this;
 };
 
 // override getHits function
-TimeSeries.prototype.getHits = function (key, gran, count, callback) {
+TimeSeries.prototype.getHits = function(key, gran, count, callback) {
   var properties = this.granularities[gran],
-    currentTime = getCurrentTime();
+      currentTime = getCurrentTime();
 
   if (typeof properties === "undefined") {
-    return callback(new Error("Unsupported granularity: " + gran));
+    return callback(new Error("Unsupported granularity: "+gran));
   }
-
-  log.info("jack test", currentTime);
 
   if (count > properties.ttl / properties.duration) {
-    return callback(new Error("Count: " + count + " exceeds the maximum stored slots for granularity: " + gran));
+    return callback(new Error("Count: "+count+" exceeds the maximum stored slots for granularity: "+gran));
   }
 
-  var from = getRoundedTime(properties.duration, currentTime - count * properties.duration, true),
-    to = getRoundedTime(properties.duration, currentTime, true);
+  var from = getRoundedTime(properties.duration, currentTime - count*properties.duration),
+      to = getRoundedTime(properties.duration, currentTime);
 
-  for (var ts = from, multi = this.redis.multi(); ts <= to; ts += properties.duration) {
-    var keyTimestamp = getRoundedTime(properties.precision || properties.ttl, ts), // high prority: precision
-      tmpKey = [this.keyBase, key, gran, keyTimestamp].join(':');
+  for(var ts=from, multi=this.redis.multi(); ts<=to; ts+=properties.duration) {
+    var keyTimestamp = getRoundedTime(properties.precision || properties.ttl, ts,true), // high prority: precision
+        tmpKey = [this.keyBase, key, gran, keyTimestamp].join(':');
 
     multi.hget(tmpKey, ts);
   }
 
-  multi.exec(function (err, results) {
+  multi.exec(function(err, results) {
     if (err) {
       return callback(err);
     }
 
-    for (var ts = from, i = 0, data = []; ts <= to; ts += properties.duration, i += 1) {
+    for(var ts=from, i=0, data=[]; ts<=to; ts+=properties.duration, i+=1) {
       data.push([ts, results[i] ? parseInt(results[i], 10) : 0]);
     }
 
@@ -148,18 +147,18 @@ TimeSeries.prototype.getHits = function (key, gran, count, callback) {
 
 const timeSeries = new TimeSeries(rclient, "timedTraffic")
 timeSeries.granularities = {
-  '1minute': { ttl: timeSeries.minutes(65), duration: timeSeries.minutes(1) },
-  '15minutes': { ttl: timeSeries.hours(50), duration: timeSeries.minutes(15) },
-  '1hour': { ttl: timeSeries.days(7), duration: timeSeries.hours(1) },
-  '1day': { ttl: timeSeries.weeks(53), duration: timeSeries.days(1), precision: timeSeries.weeks(52) },
-  '1month': { ttl: timeSeries.months(24), duration: timeSeries.months(1) }
+  '1minute'  : { ttl: timeSeries.minutes(65)  , duration: timeSeries.minutes(1) },
+  '15minutes': { ttl: timeSeries.hours(50)  , duration: timeSeries.minutes(15) },
+  '1hour'    : { ttl: timeSeries.days(7)   , duration: timeSeries.hours(1) },
+  '1day'     : { ttl: timeSeries.weeks(53) , duration: timeSeries.days(1), precision:timeSeries.weeks(52) },
+  '1month'   : { ttl: timeSeries.months(24) , duration: timeSeries.months(1) }
 }
 
 const boneAPITimeSeries = new TimeSeries(rclient, "boneAPIUsage")
 boneAPITimeSeries.granularities = {
-  '1minute': { ttl: boneAPITimeSeries.minutes(60), duration: boneAPITimeSeries.minutes(1) },
-  '1hour': { ttl: boneAPITimeSeries.days(7), duration: boneAPITimeSeries.hours(1) },
-  '1day': { ttl: boneAPITimeSeries.days(30), duration: boneAPITimeSeries.days(1) },
+  '1minute'  : { ttl: boneAPITimeSeries.minutes(60)  , duration: boneAPITimeSeries.minutes(1) },
+  '1hour'    : { ttl: boneAPITimeSeries.days(7)   , duration: boneAPITimeSeries.hours(1) },
+  '1day'     : { ttl: boneAPITimeSeries.days(30) , duration: boneAPITimeSeries.days(1) },
 }
 
 // set flag
