8c20d61da5cd5008c018b478c25f5edcf62bb414
melvin <108800+MelvinTo@users.noreply.github.com>
2023-03-02 22:52:56 +0800

    bugfix

--- platform/all/hooks/after_bro/apply_profile.sh
+++ platform/all/hooks/after_bro/apply_profile.sh
@@ -2,4 +2,5 @@
 
 : ${FIREWALLA_HOME:=/home/pi/firewalla}
 
-sudo bash -c "FW_PROFILE_KEY=taskset $FIREWALLA_HOME/scripts/apply_profile.sh" &> /tmp/apply_profile-bro-run.log
+# to ensure the log file is owned by pi
+sudo bash -c "FW_PROFILE_KEY=taskset $FIREWALLA_HOME/scripts/apply_profile.sh" 2>&1 | sudo -u pi tee /tmp/apply_profile-bro-run.log
