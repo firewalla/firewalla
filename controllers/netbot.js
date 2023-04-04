18647be17c5fd4afa676d3d1417ec0bfe36be545
melvin <108800+MelvinTo@users.noreply.github.com>
2023-04-05 01:15:35 +0800

    should always change led state

--- controllers/netbot.js
+++ controllers/netbot.js
@@ -4866,10 +4866,10 @@ class netBot extends ControllerBot {
           }
         }
         await execAsync("sync");
-        await platform.ledDoneSaving().catch(() => undefined);
       } catch (err) {
         log.error("Redis background save returns error", err.message);
       }
+      await platform.ledDoneSaving().catch(() => undefined);
     }, 5000);
   }
 }
