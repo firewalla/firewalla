3d927ae1866ac24c03b1f5f5a3df9c7e2e45e4e3
melvin <108800+MelvinTo@users.noreply.github.com>
2023-05-26 12:34:58 +0800

    add dashboard

--- api/app-local.js
+++ api/app-local.js
@@ -59,6 +59,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/dashboard', require('./routes/dashboard.js'));
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- api/routes/dashboard.js
+++ api/routes/dashboard.js
@@ -20,6 +20,56 @@ const rclient = require('../../util/redis_manager.js').getRedisClientWithDB1();
 const HostTool = require('../../net2/HostTool');
 const hostTool = new HostTool();
 
+const CloudWrapper = require('../lib/CloudWrapper');
+const cloudWrapper = new CloudWrapper();
+
+const gid = (await jsReadFile("/home/pi/.firewalla/ui.conf")).gid;
+
+async function get_latency(mac) {
+    const body = {
+        "message": {
+            "from": "iRocoX",
+            "obj": {
+                "mtype": "set",
+                "id": "DA45C7BE-9029-4165-AD56-7860A9A3AE6B",
+                "data": {
+                    "value":
+                        { 
+                            "type": "host", 
+                            "target": mac, 
+                            "queries": { "latency": {} } 
+                        },
+                    "item": "liveStats"
+                },
+                "type": "jsonmsg",
+                "target": "0.0.0.0"
+            },
+            "appInfo": {
+                "appID": "com.rottiesoft.circle",
+                "version": "1.25",
+                "platform": "ios"
+            },
+            "msg": "",
+            "type": "jsondata",
+            "compressMode": 1,
+            "mtype": "msg"
+        },
+        "mtype": "msg"
+    }
+
+    const controller = await cloudWrapper.getNetBotController(gid);
+    const response = await controller.msgHandlerAsync(gid, body, "app");
+
+    log.info("XXX", response);
+
+    return response && 
+    response.data && 
+    response.data.latency &&
+    response.data.latency[0] &&
+    response.data.latency[0].latency
+
+}
+
 /* GET home page. */
 router.get('/json/stats.json', async (req, res, next) => {
     const keys = await rclient.keysAsync('assets:status:*');
@@ -34,6 +84,7 @@ router.get('/json/stats.json', async (req, res, next) => {
             const name = hostTool.getHostname(entry);
             station.ip = ip;
             station.name = name;
+            station.latency = await get_latency(mac);
             stations.push(station);
         } catch(err) {
             log.error("Got error when process station: " + key + " " + err);
