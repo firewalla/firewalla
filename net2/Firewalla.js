f0d0081d478990f35278c0a3f3b5b5c66fef30ed
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2023-08-24 12:58:26 +0800

    Revert "add commit ts"
    This reverts commit 2a01b6ed18bd776f81f9012b6e678c43a6cb7f07.

--- net2/Firewalla.js
+++ net2/Firewalla.js
@@ -343,7 +343,6 @@ async function getLocalCommitHash() {
 async function getRemoteCommitHash() {
   // @{u}: remote-tracking branch
   // https://www.git-scm.com/docs/gitrevisions
-  await exec("git fetch origin")
   const cmd = await exec("git rev-parse @{u}")
   return cmd.stdout.trim()
 }
