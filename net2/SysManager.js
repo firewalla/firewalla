c4623cd9be99e09b507382b3ae4b7644ca73682c
Reinhard L <callmerein@gmail.com>
2023-09-12 13:14:30 +0800

    add autoUpgrade to checkin

--- net2/SysManager.js
+++ net2/SysManager.js
@@ -35,6 +35,7 @@ const LRU = require('lru-cache');
 const platformLoader = require('../platform/PlatformLoader.js');
 const platform = platformLoader.getPlatform();
 const Mode = require('./Mode.js');
+const upgradeManager = require('../net2/UpgradeManager.js');
 
 const exec = require('child-process-promise').exec
 
@@ -971,6 +972,7 @@ class SysManager {
     } catch (err) {
       log.error("Failed to load repo info from /tmp", err);
     }
+    const autoUpgrade = await upgradeManager.getAutoUpgradeState()
     // ======== end of statics =========
 
     // TODO: support v6
@@ -992,6 +994,7 @@ class SysManager {
       repoBranch: this.repo.branch,
       repoHead: this.repo.head,
       repoTag: this.repo.tag,
+      autoUpgrade,
       language: this.language,
       timezone: this.timezone,
       memory,
