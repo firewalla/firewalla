252b5cb2955d6659e1b627d842c6112ed0bd9681
melvin <108800+MelvinTo@users.noreply.github.com>
2023-11-17 13:59:19 +0800

    refine script

--- scripts/test_wan.sh
+++ scripts/test_wan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 
-# $ sudo ./test_wan.sh vpn_d7 "curl -s -m 5 -o /dev/null -I -w '%{http_code}' https://1.1.1.1"
+# $ sudo ./test_wan.sh vpn_d7 curl -s -m 5 -o /dev/null -I -w '%{http_code}' https://1.1.1.1
 # 200
 # $ curl -s -m 5 -o /dev/null -I -w '%{http_code}' https://1.1.1.1
 # 000
@@ -14,7 +14,8 @@ source "${FIREWALLA_HOME}/platform/platform.sh"
 test -z $CGROUP_SOCK_MARK && exit 1
 
 WAN_NAME=$1
-CMD="$2"
+shift
+CMD="$@"
 
 MARK=$(ip rule list | grep fwmark | grep -w vpn_client_${WAN_NAME} | awk '{print $5}' | grep -vw "iif lo" | awk -F/ '{print $1}')
 if test -z "$MARK"; then
