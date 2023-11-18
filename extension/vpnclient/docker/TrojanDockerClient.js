0a99e754fd3d149dd558cddc7f4fe07f636a8f9a
melvin <108800+MelvinTo@users.noreply.github.com>
2023-11-18 11:58:37 +0800

    disable evaluate quality

--- extension/vpnclient/docker/TrojanDockerClient.js
+++ extension/vpnclient/docker/TrojanDockerClient.js
@@ -64,6 +64,8 @@ class TrojanDockerClient extends DockerBaseVPNClient {
   }
 
   async _evaluateQuality() {
+    // temporarily comment out
+    return;
     const script = `${f.getFirewallaHome()}/scripts/test_vpn_docker.sh`;
     const intf = this.getInterfaceName();
     const rtId = await vpnClientEnforcer.getRtId(this.getInterfaceName());
