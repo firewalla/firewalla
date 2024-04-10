13667bf4a69c2a966c2aba75724bf58be4a18498
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2024-04-10 11:08:36 +0800

    add timestamp into alarm notif mutable content whitelist keys

--- controllers/netbot.js
+++ controllers/netbot.js
@@ -304,7 +304,7 @@ class netBot extends ControllerBot {
       }
 
       const alarmData = {};
-      const appUsedKeys = ["type", "p.device.name", "p.device.ip", "p.device.id",
+      const appUsedKeys = ["type", "timestamp", "p.device.name", "p.device.ip", "p.device.id",
         "p.transfer.outbound.size", "p.transfer.inbound.size", // abnormal/large upload
         "p.noticeType", "p.dest.name", "p.dest.ip", "p.device.real.ip", "p.vpnType", "p.device.mac", "p.tag.names", "p.utag.names",
         "p.dest.isLocal", "p.transfer.duration", "p.security.primaryReason", "p.local_is_client", "p.result_method", "p.result", "p.intf.desc",
