674cab5127f69c77410ba647a726c6f481650af6
Jian Yang <pub@yangjian.net>
2024-08-19 02:02:05 -0700

    update show_syslog_reboots.sh to distinguish hang/power off from reboot

--- scripts/show_syslog_reboots.sh
+++ scripts/show_syslog_reboots.sh
@@ -2,14 +2,14 @@
 
 CMD=$(basename $0)
 : ${NUM_REBOOTS:=10}
-TARGET_DIR=${1:-'/var/log'}
+: ${THRESHOLD_HANG:=100}
 
 show_reboot() {
     cat <<EOT
 
 ==> $1 <==
 EOT
-    sudo zgrep -anP '(\x00+|Booting (Linux|paravirtualized kernel)|FIREONBOOT.UPGRADE.DATE.SYNC.DONE)' $1 | grep -A1  -aP '(\x00+|Booting)' | sed 's/\x0\x0*/NULLS/g' | reformat
+    sudo zgrep -anP '(\x00+|fake-hwclock.data|FIREONBOOT.UPGRADE.DATE.SYNC.DONE)' -B1 $1 | sed 's/\x0\x0*/NULLS/g' | reformat
 }
 
 reformat() {
@@ -20,23 +20,41 @@ reformat() {
     do
     #echo ">>line<<"; echo "$line"
         case "$line" in
+            \-\-)
+              continue
+              ;;
           *NULLS*)
               lno=$(echo "$line" | awk -F: '{print $1}')
               boot='power cycle'
               ;;
-          *Booting*)
-              lno=$(echo "$line" | awk -F: '{print $1}')
-              : ${boot:=reboot}
-              ;;
           *FIREONBOOT.UPGRADE.DATE.SYNC.DONE*)
               ts=$(echo "$line" | sed 's/[0-9]*:\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
               ;;
+          *'Unable to read saved clock information: /data/fake-hwclock.data'*)
+              lno=$(echo "$line" | awk -F: '{print $1}')
+              this_ts=$(echo "$line" | sed 's/[0-9]*:\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
+              ts=$this_ts
+              this_ts=$(date +%s -d "$this_ts")
+              last_ts=$(echo "$last_line" | sed 's/[0-9]*-\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
+              if [[ -n "$last_ts" ]]; then
+                  ts=$last_ts
+                  last_ts=$(date +%s -d "$last_ts")
+                  if (( this_ts - last_ts > THRESHOLD_HANG )); then
+                      boot='hang/power off'
+                  else
+                      boot=reboot
+                  fi
+              else
+                  boot='hang/power off'
+              fi
+	      ;;
         esac
         if [[ -n "$ts" && -n "$boot" ]]
         then
             printf "%d\t%s\t%s\n" "$lno" "$ts" "$boot"
             lno= ; boot= ; ts=
         fi
+        last_line=$line
     done | tail -$NUM_REBOOTS
 }
 
@@ -49,10 +67,11 @@ EOU
 while getopts ":hm:" opt
 do
     case $opt in
-        m) NUM_REBOOTS=$OPTARG ;;
-	h) usage; exit 0 ;;
+        m) NUM_REBOOTS=$OPTARG ; shift 2 ;;
+        h) usage; exit 0 ;;
     esac
 done
+TARGET_DIR=${1:-'/var/log'}
 
 for f in $(ls -tr $TARGET_DIR/syslog*)
 do
