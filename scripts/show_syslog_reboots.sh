3551f96fdc4eaf7ad58f28ada47bc365c070b34f
Jian Yang <pub@yangjian.net>
2024-08-20 01:23:16 -0700

    update show_syslog_reboots.sh

--- scripts/show_syslog_reboots.sh
+++ scripts/show_syslog_reboots.sh
@@ -4,57 +4,62 @@ CMD=$(basename $0)
 : ${NUM_REBOOTS:=10}
 : ${THRESHOLD_HANG:=900}
 
+err() {
+    echo "ERROR: $@" >&2
+}
+
 show_reboot() {
     cat <<EOT
 
 ==> $1 <==
 EOT
-    sudo zgrep -anP '(\x00+|fake-hwclock.data|FIREONBOOT.UPGRADE.DATE.SYNC.DONE)' -B1 $1 | sed 's/\x0\x0*/NULLS/g' | reformat
+    zgrep -anP '(\x00+|fake-hwclock.data)' $1 | sed 's/\x0\x0*/NULLS/g' | reformat $1
 }
 
 reformat() {
+    file=$1
+    case $file in
+        *.gz) CAT=zcat ;;
+        *) CAT=cat ;;
+    esac
     printf "\nLINENO\tTIMESTAMP\tNOTE\n"
     echo '-------------------------------------'
-    lno= ; ts= ;boot=
+    lno= ; boot= ; last_ts= ; next_ts=
     while read line
     do
     #echo ">>line<<"; echo "$line"
         case "$line" in
-            \-\-)
-              continue
-              ;;
           *NULLS*)
-              lno=$(echo "$line" | awk -F: '{print $1}')
+              boot_lno=$(echo "$line" | awk -F: '{print $1}')
+              boot_ts=$(echo "$line" | awk '{print $1" "$2" "$3}' | sed 's/[0-9]*://')
+              last_date=$($CAT $file | head -$boot_lno | cut -d: -f1 | uniq | tail -2 |head -1)
+              last_ts=$($CAT $file | head -$boot_lno | cut -d\  -f1-3 | uniq | fgrep -a "$last_date" | tail -1)
+              lno=$($CAT $file|head -$boot_lno | fgrep -an "$last_ts" | tail -1|awk -F: '{print $1}')
               boot='power cycle'
               ;;
-          *FIREONBOOT.UPGRADE.DATE.SYNC.DONE*)
-              ts=$(echo "$line" | sed 's/[0-9]*:\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
-              ;;
           *'Unable to read saved clock information: /data/fake-hwclock.data'*)
-              lno=$(echo "$line" | awk -F: '{print $1}')
-              this_ts=$(echo "$line" | sed 's/[0-9]*:\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
-              ts=$this_ts
-              this_ts=$(date +%s -d "$this_ts")
-              last_ts=$(echo "$last_line" | sed 's/[0-9]*-\(.*\([ :][0-9][0-9]\)\{3\}\).*/\1/')
-              if [[ -n "$last_ts" ]]; then
-                  ts=$last_ts
-                  last_ts=$(date +%s -d "$last_ts")
-                  if (( this_ts - last_ts > THRESHOLD_HANG )); then
+              boot_lno=$(echo "$line" | awk -F: '{print $1}')
+              boot_ts=$(echo "$line" | awk '{print $1" "$2" "$3}' | sed 's/[0-9]*://')
+              last_date=$($CAT $file | head -$boot_lno | cut -d: -f1 | uniq | tail -2 |head -1)
+              last_ts=$($CAT $file|head -$boot_lno | cut -d\  -f1-3 | uniq | fgrep -a "$last_date" | tail -1)
+              lno=$($CAT $file|head -$boot_lno | fgrep -an "$last_ts" | tail -1|awk -F: '{print $1}')
+              next_ts=$($CAT $file | sed -n "$boot_lno,\$p" |fgrep -am 1 FIREONBOOT.UPGRADE.DATE.SYNC.DONE | cut -d\  -f1-3)
+              if [[ -n "$last_ts" && -n "$next_ts" ]]; then
+                  last_ts_epoch=$(date +%s -d "$last_ts")
+                  next_ts_epoch=$(date +%s -d "$next_ts")
+                  if (( next_ts_epoch - last_ts_epoch > THRESHOLD_HANG )); then
                       boot='hang/power off'
                   else
-                      boot=reboot
+                      boot='reboot'
                   fi
-              else
-                  boot='hang/power off'
               fi
-	      ;;
+          ;;
         esac
-        if [[ -n "$ts" && -n "$boot" ]]
+        if [[ -n "$lno" && -n "$boot" && -n "$last_ts" ]]
         then
-            printf "%d\t%s\t%s\n" "$lno" "$ts" "$boot"
-            lno= ; boot= ; ts=
+            printf "%d\t%s\t%s\n" "$lno" "$last_ts" "$boot"
+            lno= ; boot= ; last_ts=
         fi
-        last_line=$line
     done | tail -$NUM_REBOOTS
 }
 
@@ -62,6 +67,11 @@ usage() {
     cat <<EOU
 usage: $CMD [-m <max_number_of_reboots_to_show>]
 EOU
+}
+
+test $UID -eq 0 || {
+   err must run with root privilege
+   exit 1
 }
 
 while getopts ":hm:" opt
@@ -77,3 +87,4 @@ for f in $(ls -tr $TARGET_DIR/syslog*)
 do
     show_reboot $f
 done
+
