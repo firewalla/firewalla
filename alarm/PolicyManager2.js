178892511f1ba7a98c8cc4b3864d58411fd73739
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2025-01-21 15:52:52 +0800

    fix logix condition in deleteTagRelatedPolicies

--- alarm/PolicyManager2.js
+++ alarm/PolicyManager2.js
@@ -734,11 +734,11 @@ class PolicyManager2 {
     let policyKeys = [];
 
     for (let rule of rules) {
-      if (_.isEmpty(rule.tag) || rule.type !== "tag") continue;
+      if (_.isEmpty(rule.tag) && rule.type !== "tag") continue;
 
       for (const type of Object.keys(Constants.TAG_TYPE_MAP)) {
         const tagUid = Constants.TAG_TYPE_MAP[type].ruleTagPrefix + tag;
-        if (rule.tag.some(m => m == tagUid)) {
+        if (_.isArray(rule.tag) && rule.tag.some(m => m == tagUid)) {
           if (rule.tag.length <= 1) {
             policyIds.push(rule.pid);
             policyKeys.push('policy:' + rule.pid);
@@ -754,12 +754,12 @@ class PolicyManager2 {
             log.info('remove scope from policy:' + rule.pid, tag);
           }
         }
-        if (rule.type === "tag" && rule.target == tag) {
-          this.tryPolicyEnforcement(rule, 'unenforce');
-          policyIds.push(rule.pid);
-          policyKeys.push(`policy:${rule.pid}`);
-        }
-      }      
+      }
+      if (rule.type === "tag" && rule.target == tag) {
+        this.tryPolicyEnforcement(rule, 'unenforce');
+        policyIds.push(rule.pid);
+        policyKeys.push(`policy:${rule.pid}`);
+      }  
     }
 
     if (policyIds.length) {
