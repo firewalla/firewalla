0db45c73711ff3a2e0aa5dfd33daccaa9cce9365
Melvin <melvinto@gmail.com>
2025-04-07 13:43:16 +0800

    add gost

--- extension/vpnclient/VPNClient.js
+++ extension/vpnclient/VPNClient.js
@@ -95,7 +95,7 @@ class VPNClient {
   }
 
   static async getVPNProfilesForInit() {
-    const types = ["openvpn", "wireguard", "ssl", "zerotier", "nebula", "trojan", "clash", "hysteria", "ipsec", "ts"];
+    const types = ["openvpn", "wireguard", "ssl", "zerotier", "nebula", "trojan", "clash", "hysteria", "gost", "ipsec", "ts"];
     const results = {}
     await Promise.all(types.map(async (type) => {
       const c = this.getClass(type);
