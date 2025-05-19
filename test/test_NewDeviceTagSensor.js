885df9aad412c4a61321a2d4a5f6dcec5042605c
caixingcheng <cheney.success@gmail.com>
2025-05-19 11:18:15 +0800

    refine UT and mock dependencies

--- test/test_NewDeviceTagSensor.js
+++ test/test_NewDeviceTagSensor.js
@@ -1,6 +1,6 @@
 /*    Copyright 2016-2025 Firewalla Inc.
  *
- *    This program is free software: you can redistribute it and/or  modify
+ *    ;This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
  *    as published by the Free Software Foundation.
  *
@@ -13,82 +13,131 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const assert = require('chai').assert;
-const NewDeviceTagSensor = require("../sensor/NewDeviceTagSensor.js");
 
 
-describe('test NewDeviceTagSensor class', async () => {
+const mock = require('mock-require');
+const { expect, assert } = require('chai');
 
-  it('isFirewallaAP should return true when called with host MAC start with 20:6D:31:61', async() => {
-    const hostObj = {
+
+mock('../net2/logger.js', () => ({
+  info: () => {},
+  warn: () => {},
+  error: () => {}
+}));
+
+mock('../sensor/Sensor.js', {
+  Sensor: class {}
+});
+
+mock('../sensor/SensorEventManager.js', {
+  getInstance: () => ({
+    on: () => {},
+    emit: () => {}
+  })
+});
+
+mock('../net2/MessageBus.js', class {
+  constructor() {}
+  subscribe() {}
+  publish() {}
+});
+
+mock('../net2/config.js', { get: () => {} });
+mock('../net2/HostManager.js', class {});
+mock('../net2/SysManager.js', {});
+mock('../net2/NetworkProfileManager.js', {});
+mock('../alarm/Alarm.js', {});
+mock('../alarm/AlarmManager2.js', class {});
+mock('../alarm/PolicyManager2.js', class {
+  constructor() {}
+});
+mock('../util/util.js', {
+  getPreferredBName: () => '',
+  delay: async () => {}
+});
+mock('lodash', require('lodash'));
+// mock('../net2/Constants.js', {});
+mock('../net2/TagManager.js', {});
+
+
+const NewDeviceTagSensor = require('../sensor/NewDeviceTagSensor');
+const sensorInstance = new NewDeviceTagSensor({});
+
+describe('NewDeviceTagSensor.isFirewallaAP', () => {
+
+  it('should return true for Firewalla AP MAC address "20:6D:31:61"', () => {
+    const host = {
       o: {
-        mac: "20:6D:31:61:00:00",
-        dhcpName: "test"
+        mac: '20:6D:31:61:CC:CC',
+        dhcpName: 'notfirewalla'
       }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, true);
-    });
+    }
+    const result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, true);
   });
 
-  it('isFirewallaAP should return true when called with host MAC start with 20:6D:31:71', async() => {
-    const hostObj = {
+  it('should return true for Firewalla Ceiling AP MAC address, perfix "20:6D:31:71" ', () => {
+    const host = {
       o: {
-        mac: "20:6D:31:71:00:00",
-        dhcpName: "test"
+        mac: '20:6D:31:71:CC:CC',
+        dhcpName: 'notfirewalla'
       }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, true);
-    });
+    }
+    const result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, true);
   });
 
-  it('isFirewallaAP should return true when called with host MAC start with 20:6D:31 and dhcpName FirewallaAP', async() => {
-    const hostObj = {
+  it('should return false for non-Firewalla MAC address', () => {
+    const host = {
       o: {
-        mac: "20:6D:31:71:00:00",
-        dhcpName: "FirewallaAP"
+        mac: '00:11:22:33:44:55',
+        dhcpName: 'notfirewalla'
       }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, true);
-    });
+    }
+    const result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, false);
   });
 
-  it('isFirewallaAP should return true when called with host MAC start with 20:6D:31 and dhcpLeaseName FirewallaAP', async() => {
-    const hostObj = {
+  it('should return ture for these APs whose MAC address is start with "20:6D:31" and dhcpName or dhcpLeaseName is FirewallaAP', () => {
+    let host = {
       o: {
-        mac: "20:6D:31:71:00:00",
+        mac: '20:6D:31:XX:XX:XX',
+        dhcpName: 'FirewallaAP'
+      }
+    }
+    let result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, true);
+    host = {
+      o: {
+        mac: '20:6D:31:XX:XX:XX',
+        dhcpLeaseName: 'notFirewallaAP',
         "dnsmasq.dhcp.leaseName": "FirewallaAP"
       }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, true);
-    });
+    }
+    result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, true);
+
   });
 
-  it('isFirewallaAP should return false when called with host MAC start with 20:6D:33 and dhcpLeaseName FirewallaAP', async() => {
-    const hostObj = {
+ it('should return false for these APs whose MAC address is start with "20:6D:31" and dhcpName or dhcpLeaseName is not FirewallaAP', () => {
+    let host = {
       o: {
-        mac: "20:6D:31:71:00:00",
-        "dnsmasq.dhcp.leaseName": "FirewallaAP"
+        mac: '20:6D:31:XX:XX:XX',
+        dhcpName: 'notFirewallaAP'
       }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, false);
-    });
-  });
+    }
+    let result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, false);
+    host = {
+      o: {
+        mac: '20:6D:31:XX:XX:XX',
+        dhcpLeaseName: 'notFirewallaAP',
+        "dnsmasq.dhcp.leaseName": "notFirewallaAP"
+      }
+    }
+    result = sensorInstance.isFirewallaAP(host);
+    assert.equal(result, false);
 
-  it('isFirewallaAP should return false when called with host MAC start with 20:6D:33 and dhcpName FirewallaAP', async() => {
-    const hostObj = {
-      o: {
-        mac: "20:6D:31:71:00:00",
-        "dhcpName": "FirewallaAP"
-      }
-    };
-    NewDeviceTagSensor.isFirewallaAP(hostObj).then((result) => {
-      assert.equal(result, false);
-    });
   });
 
 });
