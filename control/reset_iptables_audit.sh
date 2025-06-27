9ef75a08ebd3f80748c0c9a3b6981d6ebaa897fc
Reinhard L <callmerein@gmail.com>
2025-06-27 18:06:23 +0800

    remove UPNP nat rules on reseting policies

--- control/reset_iptables_audit.sh
+++ control/reset_iptables_audit.sh
@@ -19,9 +19,9 @@ create_filter_table
 sudo iptables-restore "$iptables_file"
 sudo ip6tables-restore "$ip6tables_file"
 
-
 # as allow rules are removed, we remove registered upnp services as well.
 # firerouter_upnp@* services are always running, restart is fine
+sudo iptables -w -S -tnat |grep "\-N UPNP_" | cut -d' ' -f2| while read -r chain; do sudo iptables -w -tnat -F $chain; done
 sudo rm /var/run/upnp.*.leases
 sudo systemctl restart firerouter_upnp*
 
