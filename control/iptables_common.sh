ef7998130f05da6a676b3442ded9a99f7fc62a8f
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2025-08-12 16:16:18 +0800

    bypass disturb chain if acl is disabled

--- control/iptables_common.sh
+++ control/iptables_common.sh
@@ -693,6 +693,11 @@ cat << EOF
 # qos chain for App Disturb feature which is not controlled by FW_QOS_SWITCH
 -N FW_DISTURB_QOS
 -A FW_FORWARD -j FW_DISTURB_QOS
+# bypass disturb chain for acl off devices/networks
+-A FW_DISTURB_QOS -m set --match-set acl_off_set src,src -j CONNMARK --set-xmark 0x0/0x40000000
+-A FW_DISTURB_QOS -m set --match-set acl_off_set dst,dst -j CONNMARK --set-xmark 0x0/0x40000000
+-A FW_DISTURB_QOS -m set --match-set acl_off_set src,src -j RETURN
+-A FW_DISTURB_QOS -m set --match-set acl_off_set dst,dst -j RETURN
 -N FW_DISTURB_QOS_GLOBAL
 -A FW_DISTURB_QOS -j FW_DISTURB_QOS_GLOBAL
 -N FW_DISTURB_QOS_NET_G
@@ -703,11 +708,10 @@ cat << EOF
 -A FW_DISTURB_QOS -j FW_DISTURB_QOS_DEV_G
 -N FW_DISTURB_QOS_DEV
 -A FW_DISTURB_QOS -j FW_DISTURB_QOS_DEV
--A FW_DISTURB_QOS -m connmark ! --mark 0x0/0x40000000 -j RETURN
 
 
 -N FW_QOS_SWITCH
--A FW_DISTURB_QOS -j FW_QOS_SWITCH
+-A FW_FORWARD -m connmark --mark 0x0/0x40000000 -j FW_QOS_SWITCH
 # bit 16 - 29 in connmark indicates if packet should be mirrored to ifb device in tc filter.
 # the packet will be mirrored to ifb only if these bits are non-zero
 -A FW_QOS_SWITCH -m set --match-set qos_off_set src,src -j CONNMARK --set-xmark 0x00000000/0x3fff0000
