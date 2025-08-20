58f1334940901a9321467b4458bdda258b2e3d10
Yuchen Liu <jasonlyc.sjtu@gmail.com>
2025-08-20 17:20:11 +0800

    remove apt cache and lists after apt update

--- scripts/cron/01_update_tzdata.sh
+++ scripts/cron/01_update_tzdata.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-tzdata_ts_file=/dev/shm/tzdata_update_ts
+tzdata_ts_file=/dev/shm/tzdata_apt_update_ts
 now=$(date +%s)
 if [[ ! -e $tzdata_ts_file ]] || (( $(cat $tzdata_ts_file) < $now - 86400 * 30 )); then
   sudo dpkg --configure -a
   sudo timeout 60 \apt update
   sudo timeout 60 \apt install -y tzdata
   echo $now > $tzdata_ts_file
+  sudo apt clean
+  sudo rm -rf /log/apt/lib/lists/*
 fi
