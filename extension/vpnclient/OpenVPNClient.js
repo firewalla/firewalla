5f006fa57e08ffeb6dfa912a005e08d894a43273
caixingcheng <cheney.success@gmail.com>
2025-08-29 18:36:40 +0800

    remove ipv6 temp files in destroy

--- extension/vpnclient/OpenVPNClient.js
+++ extension/vpnclient/OpenVPNClient.js
@@ -29,7 +29,6 @@ const exec = require('child-process-promise').exec;
 const iptool = require('ip');
 const crypto = require('crypto');
 const Address6 = require('ip-address').Address6;
-const ipTool = require('ip');
 const sysManager = require('../../net2/SysManager');
 
 const SERVICE_NAME = "openvpn_client";
@@ -488,7 +487,11 @@ class OpenVPNClient extends VPNClient {
 
   async destroy() {
     await super.destroy();
-    const filesToDelete = [this._getProfilePath(), this._getRuntimeProfilePath(), this._getUserPassPath(), this._getPasswordPath(), this._getGatewayFilePath(), this._getPushOptionsPath(), this._getSubnetFilePath(), this._getIP4FilePath()];
+    const filesToDelete = [
+      this._getProfilePath(), this._getRuntimeProfilePath(), this._getUserPassPath(), this._getPasswordPath(), 
+      this._getGatewayFilePath(), this._getPushOptionsPath(), this._getSubnetFilePath(), this._getIP4FilePath(),
+      this._getGatewayFilePath(6), this._getSubnetFilePath(6), this._getIP6FilePath()
+    ];
     for (const file of filesToDelete)
       await fs.unlinkAsync(file).catch((err) => {});
     await this._cleanupLogFiles();
