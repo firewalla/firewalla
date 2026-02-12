3cde4b91095b1d7f449d4fb5b7d179920f371e1e
Reinhard L <callmerein@gmail.com>
2026-02-12 16:06:34 +0800

    fix various issues in cron scripts

--- scripts/cron/02_update_tzdata.sh
+++ scripts/cron/02_update_tzdata.sh
@@ -3,9 +3,9 @@
 tzdata_ts_file=/dev/shm/tzdata_apt_update_ts
 now=$(date +%s)
 if [[ ! -e $tzdata_ts_file ]] || (( $(cat $tzdata_ts_file) < $now - 86400 * 30 )); then
-  sudo dpkg --configure -a
-  sudo timeout 60 \apt update
-  sudo timeout 60 \apt install -y tzdata
+  sudo timeout 10 dpkg --configure -a --force-confold
+  sudo timeout 60 apt update
+  sudo timeout 60 apt install -o Dpkg::Options::="--force-confold" -y tzdata
   echo $now > $tzdata_ts_file
   sudo apt clean
   sudo rm -rf /log/apt/lib/lists/*
