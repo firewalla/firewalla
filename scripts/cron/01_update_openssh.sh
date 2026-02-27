efcb2c96e44b0ebd45ff5638f618e60c9c3b52b5
Reinhard L <callmerein@gmail.com>
2026-02-27 18:41:56 +0800

    extend timeout for apt jobs on pse

--- scripts/cron/01_update_openssh.sh
+++ scripts/cron/01_update_openssh.sh
@@ -14,13 +14,13 @@ if [[ -e $openssh_ts_file ]] && (( $(cat $openssh_ts_file) > $(date +%s) - 86400
   exit 0
 fi
 
-sudo timeout 60 apt update
+sudo timeout 600 apt update
 pkgName="openssh-server"
 
 if apt list $pkgName --upgradable 2>/dev/null | grep -q security; then
   logger "$TAG:START"
-  sudo timeout 10 dpkg --configure -a --force-confold
-  sudo timeout 60 apt install -o Dpkg::Options::="--force-confold" -y $pkgName
+  sudo timeout 30 dpkg --configure -a --force-confold
+  sudo timeout 180 apt install -o Dpkg::Options::="--force-confold" -y $pkgName
   sudo systemctl daemon-reload
   sudo systemctl restart sshd
   date +%s > $openssh_ts_file
