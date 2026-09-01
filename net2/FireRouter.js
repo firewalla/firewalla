6dfa3f2366673d2b2bac7b98afa1777f31534496
Rhett Trappman <rhett.trapp@gmail.com>
2026-09-01 17:09:52 -0600

    Fix indentation in FireRouter.js

--- net2/FireRouter.js
+++ net2/FireRouter.js
@@ -324,7 +324,7 @@ async function generateNetworkInfo(interfaceMap = intfNameMap) {
 
     if (intf.config.vid) {
       redisIntf.vid = intf.config.vid
-      } else if (intfName.startsWith("br") && Array.isArray(intf.config.intf) && !_.isEmpty(intf.config.intf)) {
+    } else if (intfName.startsWith("br") && Array.isArray(intf.config.intf) && !_.isEmpty(intf.config.intf)) {
       const vid = _.get(interfaceMap, [intf.config.intf[0], 'config', 'vid']);
       if (vid) redisIntf.vid = vid
     }
